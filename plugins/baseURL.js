3105b70292c8d764688d35a1bf54c56e62491812
Ratatinator97 <asidiras.csi@gmail.com>
2020-10-08 18:18:32 +0200

    Try hard

--- plugins/baseURL.js
+++ plugins/baseURL.js
@@ -4,6 +4,8 @@ export default ({
   $config
 }) => {
   console.log($config.baseURL);
+  console.log($config.httpURL);
+  console.log($config.httpsURL);
   axios.defaults.baseURL = $config.baseURL;
   console.log(axios.defaults.baseURL);
 }
