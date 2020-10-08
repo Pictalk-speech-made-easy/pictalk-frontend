ea78fd5f3e67318d1ae88850ef62aabfe1947d07
Ratatinator97 <asidiras.csi@gmail.com>
2020-10-08 18:25:41 +0200

    update

--- plugins/baseURL.js
+++ plugins/baseURL.js
@@ -3,9 +3,6 @@ import axios from 'axios'
 export default ({
   $config
 }) => {
-  console.log($config.baseURL);
-  console.log($config.httpURL);
-  console.log($config.httpsURL);
   axios.defaults.baseURL = $config.baseURL;
   console.log(axios.defaults.baseURL);
 }
