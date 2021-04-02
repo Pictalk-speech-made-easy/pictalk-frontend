ddc58385d33b3edbb69a5ff11a9510d00f8476d8
Ratatinator97 <asidiras.csi@gmail.com>
2021-04-02 23:36:26 +0200

    Added collections starred

--- pages/pictalk/index.vue
+++ pages/pictalk/index.vue
@@ -1,58 +1,68 @@
 <template>
-  <div class="container is-widescreen">
-    <pictoList
-      :isPicto="isPicto"
-      :pictos="loadedCollections"
-      :adminMode="isAdmin"
-    />
-  </div>
+	<div class="container is-widescreen">
+		<pictoList
+			:isPicto="isPicto"
+			:pictos="loadedCollections"
+			:adminMode="isAdmin"
+		/>
+	</div>
 </template>
 <script>
 import pictoList from "@/components/pictos/pictoList";
 import axios from "axios";
 
 export default {
-  layout: "pictalk",
-  middleware: ["check-auth", "auth", "axios"],
-  components: {
-    pictoList: pictoList
-  },
-  computed: {
-    isAdmin() {
-      if (this.$route.query.isAdmin) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-    loadedCollections() {
-      return this.$store.getters.getCollections;
-    }
-  },
-  async asyncData(context) {
-    const collections = await context.store.getters.getCollections;
-    if (collections.length !== 0) {
-      return;
-    } else {
-      try {
-        const res = await axios.get("/pictalk/collection");
-        res.data.map(collection => {
-          if (collection.path) {
-            collection.path =
-              context.$config.baseURL + "/pictalk/image/" + collection.path;
-          }
-        });
-        context.store.commit("setCollections", res.data);
-        return;
-      } catch (error) {
-        console.log("error ", error);
-      }
-    }
-  },
-  data() {
-    return {
-      isPicto: false
-    };
-  }
+	layout: "pictalk",
+	middleware: ["check-auth", "auth", "axios"],
+	components: {
+		pictoList: pictoList,
+	},
+	computed: {
+		isAdmin() {
+			if (this.$route.query.isAdmin) {
+				return true;
+			} else {
+				return false;
+			}
+		},
+		loadedCollections() {
+			let rankedCollections = [];
+			this.$store.getters.getCollections.forEach((collection) => {
+				if (collection.starred == true) {
+					rankedCollections.unshift(collection);
+				} else {
+					rankedCollections.push(collection);
+				}
+			});
+			return rankedCollections;
+		},
+	},
+	async asyncData(context) {
+		const collections = await context.store.getters.getCollections;
+		if (collections.length !== 0) {
+			return;
+		} else {
+			try {
+				const res = await axios.get("/pictalk/collection");
+				res.data.map((collection) => {
+					if (collection.path) {
+						collection.path =
+							context.$config.baseURL +
+							"/pictalk/image/" +
+							collection.path;
+					}
+				});
+				context.store.commit("setCollections", res.data);
+				return;
+			} catch (error) {
+				console.log("error ", error);
+			}
+		}
+	},
+	data() {
+		return {
+			isPicto: false,
+		};
+	},
 };
 </script>
