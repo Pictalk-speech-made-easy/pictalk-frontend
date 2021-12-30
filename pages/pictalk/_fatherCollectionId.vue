5edb87451ab56e1882da1e0956278ec29d707ce1
Ratatinator97 <asidiras.csi@gmail.com>
2021-12-30 12:40:55 +0100

    Changes from dev

--- pages/pictalk/_fatherCollectionId.vue
+++ pages/pictalk/_fatherCollectionId.vue
@@ -155,13 +155,12 @@ export default {
 <style scoped>
 .pictobar {
 	bottom: 3%;
-	min-width: 35%;
-	max-width: 100%;
+	width: 98%;
 	max-height: 20%;
 	position: fixed;
 }
 .filler {
-	padding-bottom: 35%;
+	padding-bottom: 20%;
 }
 .contenant {
 	display: flex;
