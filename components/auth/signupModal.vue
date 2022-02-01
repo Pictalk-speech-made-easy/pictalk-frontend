bce679701a0d726a46e8b939b810094ed7928671
UnlikelyBuddy1 <adrianos.sidiras@gmail.com>
2022-02-01 19:55:42 +0100

    UI changes & disabled signup button trigger

--- components/auth/signupModal.vue
+++ components/auth/signupModal.vue
@@ -1,14 +1,22 @@
 <template>
 	<form action>
-		<div class="modal-card">
+		<div class="modal-card" style="width: auto">
 			<header class="modal-card-head">
-				<p class="modal-card-title">{{ $t("SignUp") }}</p>
+				<b-button
+					class="button"
+					type="button"
+					icon-left="arrow-left"
+					@click="$parent.close()"
+				/>
+				<p align="center" class="modal-card-title">{{ $t("SignUp") }}</p>
 			</header>
 			<section class="modal-card-body">
 				<div class="container">
 				<b-steps 
+					v-model="activeStep"
 					rounded
 					animated
+					:has-navigation="false"
 					mobile-mode="compact"
 					label-position="bottom">
         <b-step-item clickable :label="$t('Account')" icon="account-key">
@@ -162,8 +170,21 @@
 				
 			</section>
 			<footer class="modal-card-foot">
-				<b-button
-					class="is-primary"
+				<div class="container">
+				<div class="columns is-mobile is-full">
+					<div class="column is-one-quarter">
+							<b-button
+								@click="previousStep()"
+								:disabled="activeStep == 0"
+								class="button center"
+								type="button"
+								icon-right="chevron-left"
+							/>
+						</div>
+						<div class="column is-half">
+							<b-button
+					class="is-primary fullWidth"
+					:disabled="!(username && password && majority && terms && passwordConfirmation)"
 					@click="
 						onSubmit(
 							username,
@@ -175,6 +196,17 @@
 					"
 					>{{ $t("SignUp") }}</b-button
 				>
+						</div>
+						<div class="column is-one-quarter">
+							<b-button
+								class="center"
+								:disabled="activeStep == 3"
+								@click="nextStep()"
+								icon-right="chevron-right"
+							/>
+						</div>
+				</div>
+				</div>
 			</footer>
 		</div>
 	</form>
@@ -185,291 +217,281 @@ import axios from "axios";
 import { countryCodeEmoji } from "country-code-emoji";
 import frenchFries from "@/assets/frenchFries.json";
 export default {
-	computed: {
-		loadedVoices() {
-			return this.voices.sort((x, y) => {
-				let a = x.lang.toUpperCase(),
-					b = y.lang.toUpperCase();
-				return a == b ? 0 : a > b ? 1 : -1;
-			});
-		},
-	},
-	data() {
-		return {
-			username: "",
-			password: "",
-			voiceURI: "",
-			voices: [],
-			voiceURIs: [],
-			terms: false,
-			majority: false,
-			passwordConfirmation: "",
-			loadingVoices: true,
-			directSharers: [],
-			showLanguages: false,
-		};
-	},
-	async created() {
-		const allVoicesObtained = new Promise(function (resolve, reject) {
-			let voices = window.speechSynthesis.getVoices();
-			if (voices.length !== 0) {
-				resolve(voices);
-			} else {
-				window.speechSynthesis.addEventListener(
-					"voiceschanged",
-					function () {
-						voices = window.speechSynthesis.getVoices();
-						resolve(voices);
-					}
-				);
-			}
-		});
-		allVoicesObtained.then((voices) => {
-			this.voices = voices;
-			console.log(voices);
-			this.voiceURI = this.voices.filter(
-				(voice) => voice.lang == this.localeIso()
-			)[0].voiceURI;
-			this.loadingVoices = false;
-		});
-	},
-	watch: {
-		voiceURI: {
-			handler: function (v) {
-				this.voiceURIs = [];
-				this.voiceURIs.push(v);
-				if (!this.loadingVoices) {
-					this.playSentenceInLanguage(
-						this.voices.filter((voice) => voice.voiceURI == v)[0]
-							.lang,
-						v
-					);
-				}
-			},
-			deep: true,
-		},
-		voiceURIs(newValue, oldValue) {
-			if (newValue.length > oldValue.length && newValue.length > 1) {
-				const v = newValue.filter(
-					(ai) => oldValue.indexOf(ai) == -1
-				)[0];
-				if (!this.loadingVoices) {
-					this.playSentenceInLanguage(
-						this.voices.filter((voice) => voice.voiceURI == v)[0]
-							.lang,
-						v
-					);
-				}
-			}
-		},
-	},
-	methods: {
-		localeIso() {
-			return this.$i18n.locales.filter(
-				(i) => i.code == this.$i18n.locale
-			)[0].iso;
-		},
-		convertToSimpleLanguage(language) {
-			return language.replace(/[^a-z]/g, "");
-		},
-		getDeviceInfo() {
-			return (
-				this.getOSInfo() +
-				window.screen.height +
-				window.screen.width +
-				window.devicePixelRatio
-			);
-		},
-		getOSInfo() {
-			if (window.navigator.userAgent.indexOf("Windows NT 10.0") != -1)
-				return "Windows 10";
-			if (window.navigator.userAgent.indexOf("Windows NT 6.3") != -1)
-				return "Windows 8.1";
-			if (window.navigator.userAgent.indexOf("Windows NT 6.2") != -1)
-				return "Windows 8";
-			if (window.navigator.userAgent.indexOf("Windows NT 6.1") != -1)
-				return "Windows 7";
-			if (window.navigator.userAgent.indexOf("Windows NT 6.0") != -1)
-				return "Windows Vista";
-			if (window.navigator.userAgent.indexOf("Mac") != -1)
-				return "Mac/iOS";
-			if (window.navigator.userAgent.indexOf("X11") != -1) return "UNIX";
-			if (window.navigator.userAgent.indexOf("Linux") != -1)
-				return "Linux";
-		},
-		async playSentenceInLanguage(lang, voiceURI) {
-			let translatedText =
-				frenchFries[this.convertToSimpleLanguage(lang)];
-			this.pronounce(translatedText, lang, voiceURI);
-		},
-		async pronounce(speech, lang, voiceURI) {
-			if ("speechSynthesis" in window) {
-				var msg = new SpeechSynthesisUtterance();
-				msg.text = speech;
-				let voice = this.voices.filter(
-					(voice) => voice.voiceURI == voiceURI
-				);
-				if (voice.length == 0) {
-					voice = this.voices.filter((voice) => voice.lang == lang);
-				}
-				if (voice.length !== 0) {
-					msg.voice = voice[0];
-				}
-				window.speechSynthesis.speak(msg);
-			} else {
-				const notif = this.$buefy.notification.open({
-					duration: 5000,
-					message: this.$t("NoVoicesFound"),
-					position: "is-top-right",
-					type: "is-warning",
-					hasIcon: true,
-				});
-			}
-		},
-		getEmoji(language) {
-			if (language?.match(/[a-z]{2}-[A-Z]{2}/g)) {
-				return countryCodeEmoji(language.split("-")[1]);
-			}
-			return;
-		},
-		async onSubmit() {
-			if (
-				this.username == "" ||
-				this.password == "" ||
-				this.passwordConfirmation == "" ||
-				this.major == false ||
-				this.terms == false ||
-				this.voiceURI == "" ||
-				this.voiceURIs.length == 0
-			) {
-				const notif = this.$buefy.notification.open({
-					duration: 5000,
-					message: this.$t("PleaseCompleteForm"),
-					position: "is-top-right",
-					type: "is-info",
-					hasIcon: true,
-					icon: "account",
-				});
-				return;
-			}
-			if (this.passwordConfirmation != this.password) {
-				const notif = this.$buefy.notification.open({
-					duration: 5000,
-					message: this.$t("PasswordNotCorrespond"),
-					position: "is-top-right",
-					type: "is-warning",
-					hasIcon: true,
-					icon: "key",
-				});
-				return;
-			}
-			//let device, language, languages = {};
-			let device = {};
-			let language = {};
-			let languages = {};
-			device[this.getDeviceInfo()] = {
-				voiceURI: this.voiceURI,
-				pitch: "",
-			};
-			language[
-				this.voices.filter(
-					(voice) => voice.voiceURI == this.voiceURI
-				)[0].lang
-			] = device;
-			Object.assign(languages, language);
-			this.voiceURIs.forEach((voiceURI) => {
-				device[this.getDeviceInfo()] = {
-					voiceURI: voiceURI,
-					pitch: "",
-				};
-				languages[
-					this.voices.filter(
-						(voice) => voice.voiceURI == voiceURI
-					)[0].lang
-				] = device;
-			});
-			try {
-				const res = await axios.post("/auth/signup", {
-					username: this.username,
-					password: this.password,
-					language: JSON.stringify(language),
-					languages: JSON.stringify(languages),
-					directSharers: this.directSharers,
-				});
-				if (res.status == 201) {
-					await this.$store.dispatch("authenticateUser", {
-						username: this.username,
-						password: this.password,
-						isLogin: true,
-					});
-					const notif = this.$buefy.notification.open({
-						duration: 5000,
-						message: this.$t("AccountCreated"),
-						position: "is-top-right",
-						type: "is-success",
-						hasIcon: true,
-					});
-					this.$parent.close();
-					this.$router.push("/pictalk");
-				}
-			} catch (error) {
-				if (error.response) {
-					if (
-						error.response.status == 400 ||
-						error.response.status == 401
-					) {
-						const notif = this.$buefy.notification.open({
-							duration: 5000,
-							message: this.$t("ParametersInvalid"),
-							position: "is-top-right",
-							type: "is-danger",
-							hasIcon: true,
-							icon: "account",
-						});
-					} else {
-						if (error.response.status == 409) {
-							const notif = this.$buefy.notification.open({
-								duration: 5000,
-								message: this.$t("EmailAlreadyInUse"),
-								position: "is-top-right",
-								type: "is-danger",
-								hasIcon: true,
-								icon: "mail",
-							});
-						} else {
-							const notif = this.$buefy.notification.open({
-								duration: 5000,
-								message: this.$t("ServerOffline"),
-								position: "is-top-right",
-								type: "is-danger",
-								hasIcon: true,
-								icon: "account",
-							});
-						}
-					}
-				} else {
-					const notif = this.$buefy.notification.open({
-						duration: 5000,
-						message: this.$t("ServerOffline"),
-						position: "is-top-right",
-						type: "is-danger",
-						hasIcon: true,
-						icon: "account",
-					});
-				}
-			}
-		},
-	},
+  computed: {
+    loadedVoices() {
+      return this.voices.sort((x, y) => {
+        let a = x.lang.toUpperCase(),
+          b = y.lang.toUpperCase();
+        return a == b ? 0 : a > b ? 1 : -1;
+      });
+    },
+  },
+  data() {
+    return {
+      username: "",
+      password: "",
+      voiceURI: "",
+      voices: [],
+      voiceURIs: [],
+      terms: false,
+      majority: false,
+      passwordConfirmation: "",
+      loadingVoices: true,
+      directSharers: [],
+      showLanguages: false,
+      activeStep: 0,
+    };
+  },
+  async created() {
+    const allVoicesObtained = new Promise(function (resolve, reject) {
+      let voices = window.speechSynthesis.getVoices();
+      if (voices.length !== 0) {
+        resolve(voices);
+      } else {
+        window.speechSynthesis.addEventListener("voiceschanged", function () {
+          voices = window.speechSynthesis.getVoices();
+          resolve(voices);
+        });
+      }
+    });
+    allVoicesObtained.then((voices) => {
+      this.voices = voices;
+      console.log(voices);
+      this.voiceURI = this.voices.filter(
+        (voice) => voice.lang == this.localeIso()
+      )[0].voiceURI;
+      this.loadingVoices = false;
+    });
+  },
+  watch: {
+    voiceURI: {
+      handler: function (v) {
+        this.voiceURIs = [];
+        this.voiceURIs.push(v);
+        if (!this.loadingVoices) {
+          this.playSentenceInLanguage(
+            this.voices.filter((voice) => voice.voiceURI == v)[0].lang,
+            v
+          );
+        }
+      },
+      deep: true,
+    },
+    voiceURIs(newValue, oldValue) {
+      if (newValue.length > oldValue.length && newValue.length > 1) {
+        const v = newValue.filter((ai) => oldValue.indexOf(ai) == -1)[0];
+        if (!this.loadingVoices) {
+          this.playSentenceInLanguage(
+            this.voices.filter((voice) => voice.voiceURI == v)[0].lang,
+            v
+          );
+        }
+      }
+    },
+  },
+  methods: {
+    nextStep() {
+      this.activeStep += 1;
+    },
+    previousStep() {
+      this.activeStep -= 1;
+    },
+    localeIso() {
+      return this.$i18n.locales.filter((i) => i.code == this.$i18n.locale)[0]
+        .iso;
+    },
+    convertToSimpleLanguage(language) {
+      return language.replace(/[^a-z]/g, "");
+    },
+    getDeviceInfo() {
+      return (
+        this.getOSInfo() +
+        window.screen.height +
+        window.screen.width +
+        window.devicePixelRatio
+      );
+    },
+    getOSInfo() {
+      if (window.navigator.userAgent.indexOf("Windows NT 10.0") != -1)
+        return "Windows 10";
+      if (window.navigator.userAgent.indexOf("Windows NT 6.3") != -1)
+        return "Windows 8.1";
+      if (window.navigator.userAgent.indexOf("Windows NT 6.2") != -1)
+        return "Windows 8";
+      if (window.navigator.userAgent.indexOf("Windows NT 6.1") != -1)
+        return "Windows 7";
+      if (window.navigator.userAgent.indexOf("Windows NT 6.0") != -1)
+        return "Windows Vista";
+      if (window.navigator.userAgent.indexOf("Mac") != -1) return "Mac/iOS";
+      if (window.navigator.userAgent.indexOf("X11") != -1) return "UNIX";
+      if (window.navigator.userAgent.indexOf("Linux") != -1) return "Linux";
+    },
+    async playSentenceInLanguage(lang, voiceURI) {
+      let translatedText = frenchFries[this.convertToSimpleLanguage(lang)];
+      this.pronounce(translatedText, lang, voiceURI);
+    },
+    async pronounce(speech, lang, voiceURI) {
+      if ("speechSynthesis" in window) {
+        var msg = new SpeechSynthesisUtterance();
+        msg.text = speech;
+        let voice = this.voices.filter((voice) => voice.voiceURI == voiceURI);
+        if (voice.length == 0) {
+          voice = this.voices.filter((voice) => voice.lang == lang);
+        }
+        if (voice.length !== 0) {
+          msg.voice = voice[0];
+        }
+        window.speechSynthesis.speak(msg);
+      } else {
+        const notif = this.$buefy.notification.open({
+          duration: 5000,
+          message: this.$t("NoVoicesFound"),
+          position: "is-top-right",
+          type: "is-warning",
+          hasIcon: true,
+        });
+      }
+    },
+    getEmoji(language) {
+      if (language?.match(/[a-z]{2}-[A-Z]{2}/g)) {
+        return countryCodeEmoji(language.split("-")[1]);
+      }
+      return;
+    },
+    async onSubmit() {
+      if (
+        this.username == "" ||
+        this.password == "" ||
+        this.passwordConfirmation == "" ||
+        this.major == false ||
+        this.terms == false ||
+        this.voiceURI == "" ||
+        this.voiceURIs.length == 0
+      ) {
+        const notif = this.$buefy.notification.open({
+          duration: 5000,
+          message: this.$t("PleaseCompleteForm"),
+          position: "is-top-right",
+          type: "is-info",
+          hasIcon: true,
+          icon: "account",
+        });
+        return;
+      }
+      if (this.passwordConfirmation != this.password) {
+        const notif = this.$buefy.notification.open({
+          duration: 5000,
+          message: this.$t("PasswordNotCorrespond"),
+          position: "is-top-right",
+          type: "is-warning",
+          hasIcon: true,
+          icon: "key",
+        });
+        return;
+      }
+      //let device, language, languages = {};
+      let device = {};
+      let language = {};
+      let languages = {};
+      device[this.getDeviceInfo()] = {
+        voiceURI: this.voiceURI,
+        pitch: "",
+      };
+      language[
+        this.voices.filter((voice) => voice.voiceURI == this.voiceURI)[0].lang
+      ] = device;
+      Object.assign(languages, language);
+      this.voiceURIs.forEach((voiceURI) => {
+        device[this.getDeviceInfo()] = {
+          voiceURI: voiceURI,
+          pitch: "",
+        };
+        languages[
+          this.voices.filter((voice) => voice.voiceURI == voiceURI)[0].lang
+        ] = device;
+      });
+      try {
+        const res = await axios.post("/auth/signup", {
+          username: this.username,
+          password: this.password,
+          language: JSON.stringify(language),
+          languages: JSON.stringify(languages),
+          directSharers: this.directSharers,
+        });
+        if (res.status == 201) {
+          await this.$store.dispatch("authenticateUser", {
+            username: this.username,
+            password: this.password,
+            isLogin: true,
+          });
+          const notif = this.$buefy.notification.open({
+            duration: 5000,
+            message: this.$t("AccountCreated"),
+            position: "is-top-right",
+            type: "is-success",
+            hasIcon: true,
+          });
+          this.$parent.close();
+          this.$router.push("/pictalk");
+        }
+      } catch (error) {
+        if (error.response) {
+          if (error.response.status == 400 || error.response.status == 401) {
+            const notif = this.$buefy.notification.open({
+              duration: 5000,
+              message: this.$t("ParametersInvalid"),
+              position: "is-top-right",
+              type: "is-danger",
+              hasIcon: true,
+              icon: "account",
+            });
+          } else {
+            if (error.response.status == 409) {
+              const notif = this.$buefy.notification.open({
+                duration: 5000,
+                message: this.$t("EmailAlreadyInUse"),
+                position: "is-top-right",
+                type: "is-danger",
+                hasIcon: true,
+                icon: "mail",
+              });
+            } else {
+              const notif = this.$buefy.notification.open({
+                duration: 5000,
+                message: this.$t("ServerOffline"),
+                position: "is-top-right",
+                type: "is-danger",
+                hasIcon: true,
+                icon: "account",
+              });
+            }
+          }
+        } else {
+          const notif = this.$buefy.notification.open({
+            duration: 5000,
+            message: this.$t("ServerOffline"),
+            position: "is-top-right",
+            type: "is-danger",
+            hasIcon: true,
+            icon: "account",
+          });
+        }
+      }
+    },
+  },
 };
 </script>
 <style>
 .center {
-	display: block;
-	margin-left: auto;
-	margin-right: auto;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
 }
 .contenant {
-	display: flex;
-	justify-content: center;
+  display: flex;
+  justify-content: center;
+}
+.fullWidth {
+  width: 100%;
 }
 </style>
