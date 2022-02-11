c72ce2d51fb43ddc9bd02979a059a3c2f4e4e0e8
UnlikelyBuddy1 <adrianos.sidiras@gmail.com>
2022-02-11 19:08:47 +0100

    fixed problem on signup

--- layouts/pictalk.vue
+++ layouts/pictalk.vue
@@ -1,61 +1,59 @@
 <template>
-	<div class="wrapper">
-		<pictalkNavbar />
-		<hr />
-		<nuxt />
-		<br />
-	</div>
+  <div class="wrapper">
+    <pictalkNavbar />
+    <hr />
+    <nuxt />
+    <br />
+  </div>
 </template>
 <script>
 import pictalkNavbar from "@/components/navigation/pictalk-navbar";
 export default {
-	components: {
-		pictalkNavbar,
-	},
-	data() {
-		return {
-			intervalId: null,
-		};
-	},
-	created() {
-		if (
-			this.$store.getters.isAuthenticated &&
-			this.$store.getters.getUser &&
-			this.$store.getters.getUser.displayLanguage.match(/[a-z]{2}/g)
-		) {
-			if (
-				this.$i18n.locale.code !=
-				this.$store.getters.getUser.displayLanguage
-			) {
-				this.$i18n.setLocale(
-					this.$store.getters.getUser.displayLanguage
-				);
-			}
-		}
-	},
-	async mounted() {
-		this.intervalId = setInterval(async function () {
-			if (window.navigator.onLine) {
-				try {
-					await this.$nuxt.$store.dispatch("downloadCollections");
-					await this.$nuxt.$store.dispatch("getUser");
-				} catch (err) {
-					console.log(err);
-				}
-			}
-		}, 30000);
-	},
-	destroyed() {
-		clearInterval(this.intervalId);
-	},
+  components: {
+    pictalkNavbar,
+  },
+  data() {
+    return {
+      intervalId: null,
+    };
+  },
+  async created() {
+    await this.$store.dispatch("getUser");
+    if (
+      this.$store.getters.isAuthenticated &&
+      this.$store.getters.getUser &&
+      this.$store.getters.getUser.displayLanguage.match(/[a-z]{2}/g)
+    ) {
+      if (
+        this.$i18n.locale.code != this.$store.getters.getUser.displayLanguage
+      ) {
+        this.$i18n.setLocale(this.$store.getters.getUser.displayLanguage);
+      }
+    }
+  },
+  async mounted() {
+    this.intervalId = setInterval(async function () {
+      if (window.navigator.onLine) {
+        try {
+          await this.$nuxt.$store.dispatch("downloadCollections");
+          await this.$nuxt.$store.dispatch("getUser");
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }, 30000);
+  },
+  destroyed() {
+    clearInterval(this.intervalId);
+  },
 };
 </script>
 <style scoped>
 .wrapper {
-	overflow-x: hidden;
-	position: relative;
-	width: 100%;
-	height: 100%;
+  overflow-x: hidden;
+  position: relative;
+  width: 100%;
+  height: 100%;
 }
 </style>
 
