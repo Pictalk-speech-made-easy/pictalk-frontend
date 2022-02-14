d75f9bddbe208f4036ce9e31d9e4dce82a80a921
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2022-02-14 17:14:37 +0100

    Rework on languages => Made simplier

--- components/pictos/deleteItem.vue
+++ components/pictos/deleteItem.vue
@@ -1,118 +1,124 @@
 <template>
-  <div class="modal-card" style="width: auto">
-    <header class="modal-card-head">
-      <b-button
-        class="button"
-        type="is-danger"
-        icon-left="arrow-left"
-        @click="$parent.close()"
-      />
-      <p align="center" class="modal-card-title">{{ $t("Delete") }}</p>
-    </header>
-    <section class="modal-card-body">
-      {{ $t("DeleteItem") }}{{ object.meaning[getUserLang()] }} ?
-      <br />
-      <br />
-      <img
-        class="image"
-        :src="object.image"
-        width="40%"
-        crossorigin="anonymous"
-        style=""
-      />
-      <br />
-      <br />
-      <b-field
-        :label="
-          $t('PleaseType1') +
-          ' <' +
-          object.meaning[getUserLang()] +
-          '> ' +
-          $t('PleaseType2')
-        "
-      >
-        <b-input
-          v-model="meaningOrName"
-          :placeholder="object.meaning[getUserLang()]"
-          v-on:keyup.native.enter="object.meaning[getUserLang()]"
-        ></b-input>
-      </b-field>
-    </section>
-    <footer class="modal-card-foot">
-      <b-button class="button" type="button" @click="$parent.close()">{{
-        $t("Close")
-      }}</b-button>
-      <b-button class="button is-primary" @click="onSubmitted(meaningOrName)">{{
-        $t("Delete")
-      }}</b-button>
-    </footer>
-  </div>
+	<div class="modal-card" style="width: auto">
+		<header class="modal-card-head">
+			<b-button
+				class="button"
+				type="is-danger"
+				icon-left="arrow-left"
+				@click="$parent.close()"
+			/>
+			<p align="center" class="modal-card-title">{{ $t("Delete") }}</p>
+		</header>
+		<section class="modal-card-body">
+			{{ $t("DeleteItem") }}{{ object.meaning[getUserLang] }} ?
+			<br />
+			<br />
+			<img
+				class="image"
+				:src="object.image"
+				width="40%"
+				crossorigin="anonymous"
+				style=""
+			/>
+			<br />
+			<br />
+			<b-field
+				:label="
+					$t('PleaseType1') +
+					' <' +
+					object.meaning[getUserLang] +
+					'> ' +
+					$t('PleaseType2')
+				"
+			>
+				<b-input
+					v-model="meaningOrName"
+					:placeholder="object.meaning[getUserLang]"
+					v-on:keyup.native.enter="object.meaning[getUserLang]"
+				></b-input>
+			</b-field>
+		</section>
+		<footer class="modal-card-foot">
+			<b-button class="button" type="button" @click="$parent.close()">{{
+				$t("Close")
+			}}</b-button>
+			<b-button
+				class="button is-primary"
+				@click="onSubmitted(meaningOrName)"
+				>{{ $t("Delete") }}</b-button
+			>
+		</footer>
+	</div>
 </template>
 <script>
 export default {
-  props: {
-    object: {
-      type: Object,
-      required: true,
-    },
-  },
-  data() {
-    return {
-      meaningOrName: "",
-    };
-  },
-  methods: {
-    getUserLang(detailled = false) {
-      const user = this.$store.getters.getUser;
-      const lang = Object.keys(user.language)[0];
-      if (lang && !detailled) {
-        return lang.replace(/[^a-z]/g, "");
-      } else if (lang && detailled) {
-        return lang;
-      } else {
-        return window.navigator.language;
-      }
-    },
-    async onSubmitted(name) {
-      if (name == this.object.meaning[this.getUserLang()]) {
-        try {
-          if (this.object.collection) {
-            const res = await this.$store.dispatch("removeCollection", {
-              collectionId: this.object.id,
-              fatherCollectionId: parseInt(
-                this.$route.params.fatherCollectionId,
-                10
-              ),
-            });
-          } else {
-            const res = await this.$store.dispatch("removePicto", {
-              pictoId: this.object.id,
-              fatherCollectionId: parseInt(
-                this.$route.params.fatherCollectionId,
-                10
-              ),
-            });
-          }
-          this.$buefy.toast.open({
-            message: this.$t("DeletedSuccess"),
-            type: "is-success",
-          });
-          this.$parent.close();
-          return;
-        } catch (ex) {
-          console.log(ex);
-          this.$buefy.toast.open({
-            message: this.$t("SomeThingBadHappened"),
-            type: "is-danger",
-          });
-        }
-      } else {
-        this.$buefy.toast.open({
-          message: this.$t("DeleteNotCorrespond"),
-          type: "is-danger",
-        });
-      }
-    },
-  },
+	props: {
+		object: {
+			type: Object,
+			required: true,
+		},
+	},
+	data() {
+		return {
+			meaningOrName: "",
+		};
+	},
+	computed: {
+		getUserLang() {
+			const user = this.$store.getters.getUser;
+			if (user?.language) {
+				return Object.keys(user.language)[0].replace(/[^a-z]/g, "");
+			}
+			if (user?.displayLanguage) {
+				return user.displayLanguage;
+			}
+			return window.navigator.language.replace(/[^a-z]/g, "");
+		},
+	},
+	methods: {
+		async onSubmitted(name) {
+			if (name == this.object.meaning[this.getUserLang]) {
+				try {
+					if (this.object.collection) {
+						const res = await this.$store.dispatch(
+							"removeCollection",
+							{
+								collectionId: this.object.id,
+								fatherCollectionId: parseInt(
+									this.$route.params.fatherCollectionId,
+									10
+								),
+							}
+						);
+					} else {
+						const res = await this.$store.dispatch("removePicto", {
+							pictoId: this.object.id,
+							fatherCollectionId: parseInt(
+								this.$route.params.fatherCollectionId,
+								10
+							),
+						});
+					}
+					this.$buefy.toast.open({
+						message: this.$t("DeletedSuccess"),
+						type: "is-success",
+					});
+					this.$parent.close();
+					return;
+				} catch (ex) {
+					console.log(ex);
+					this.$buefy.toast.open({
+						message: this.$t("SomeThingBadHappened"),
+						type: "is-danger",
+					});
+				}
+			} else {
+				this.$buefy.toast.open({
+					message: this.$t("DeleteNotCorrespond"),
+					type: "is-danger",
+				});
+			}
+		},
+	},
 };
 </script>
--- components/pictos/tradLanguageList.vue
+++ components/pictos/tradLanguageList.vue
@@ -1,33 +1,66 @@
 <template>
-<div class="modal-card" style="width: auto">
+	<div class="modal-card" style="width: auto">
 		<section class="modal-card-body">
-	<div class="columns is-mobile is-multiline is-centered">
-		<div v-for="language in languages" class="column">
-			<b-button @click="setTemporaryLanguage(language)" class="is-light">{{ getEmoji(language) }}</b-button>
-		</div>
-	</div>
+			<div class="columns is-mobile is-multiline is-centered">
+				<div v-for="language in languages" class="column">
+					<b-button
+						@click="setTemporaryLanguage(language)"
+						class="is-light"
+						>{{ getEmoji(language) }}</b-button
+					>
+				</div>
+			</div>
 		</section>
-</div>
+	</div>
 </template>
 <script>
 import { countryCodeEmoji } from "country-code-emoji";
 export default {
 	data() {
 		return {
-			languages: ['fr-FR', 'en-US', 'de-DE', 'bg-BG', 'cs-CZ', 'da-DK', 'el-GR', 'es-ES', 'et-ET', 'fi-FI', 'hu-HU', 'it-IT', 'ja-JP', 'lt-LT', 'lv-LV', 'nl-NL', 'pl-PL', 'pt-PT', 'ro-RO', 'ru-RU', 'sk-SK', 'sl-SL', 'sv-SV', 'zh-HK']
-		}
+			languages: [
+				"fr-FR",
+				"en-US",
+				"de-DE",
+				"bg-BG",
+				"cs-CZ",
+				"da-DK",
+				"el-GR",
+				"es-ES",
+				"et-ET",
+				"fi-FI",
+				"hu-HU",
+				"it-IT",
+				"ja-JP",
+				"lt-LT",
+				"lv-LV",
+				"nl-NL",
+				"pl-PL",
+				"pt-PT",
+				"ro-RO",
+				"ru-RU",
+				"sk-SK",
+				"sl-SL",
+				"sv-SV",
+				"zh-HK",
+			],
+		};
 	},
 	methods: {
 		getEmoji(language) {
-			if (language?.match(/[a-z]{2}-[A-Z]{2}/g)) {
-			return countryCodeEmoji(language.split("-")[1]);
+			if (language?.match(/[a-z]{2}_[A-Z]{2}/g)) {
+				language = language.replace("_", "-");
 			}
-			return;
+			if (language?.match(/[a-z]{2}-[A-Z]{2}/g)) {
+				return countryCodeEmoji(language.split("-")[1]);
+			} else {
+				return language;
+			}
 		},
 		setTemporaryLanguage(language) {
-			this.$store.commit('setTemporaryLanguage', language);
+			this.$store.commit("setTemporaryLanguage", language);
 			this.$parent.close();
-		}
-	}
-}
+		},
+	},
+};
 </script>
