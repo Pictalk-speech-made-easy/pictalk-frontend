af186c332949c922545429f3994da3132a283704
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2022-02-26 16:41:39 +0100

    Offline mode !

--- plugins/workboxConfig.js
+++ plugins/workboxConfig.js
@@ -1,15 +1,15 @@
 workbox.routing.registerRoute(
-  new RegExp('https://apidev\\.pictalk\\.org/pictalk/image/.*\\.(png|jpg|jpeg|gif)'),
-  //new RegExp('http://localhost:3001/pictalk/image/.*\\.(png|jpg|jpeg|gif)'),
-  new workbox.strategies.CacheFirst({
-    cacheName: 'pictos',
-    plugins: [
-      new workbox.cacheableResponse.CacheableResponsePlugin({ statuses: [200] }),
-      new workbox.rangeRequests.RangeRequestsPlugin(),
-    ],
-    matchOptions: {
-      ignoreSearch: false,
-      ignoreVary: true
-    }
-  }),
+	new RegExp('https://api\\.pictalk\\.org/pictalk/image/.*\\.(png|jpg|jpeg|gif|webp)'),
+	//new RegExp('http://localhost:3001/pictalk/image/.*\\.(png|jpg|jpeg|gif)'),
+	new workbox.strategies.CacheFirst({
+		cacheName: 'pictos',
+		plugins: [
+			new workbox.cacheableResponse.CacheableResponsePlugin({ statuses: [200] }),
+			new workbox.rangeRequests.RangeRequestsPlugin(),
+		],
+		matchOptions: {
+			ignoreSearch: false,
+			ignoreVary: true
+		}
+	}),
 );
