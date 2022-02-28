f40f4da89705bc0a3c58cff912765edad2f5b951
UnlikelyBuddy1 <adrianos.sidiras@gmail.com>
2022-02-28 11:00:17 +0100

    hot fix on languageRegex being null

--- mixins/emoji.js
+++ mixins/emoji.js
@@ -7,18 +7,19 @@ export default {
 			}
 			let emojiOrText;
 			let languageRegex = language?.match(/[a-z]{2}-[A-Z]{2}/g);
-			if (languageRegex.length >= 1) {
-				languageRegex = languageRegex[0];
-				try {
-					emojiOrText = countryCodeEmoji(languageRegex.split("-")[1]);
-				} catch (err) {
-					emojiOrText = languageRegex;
-					throw new Error("Could not get emoji from language: " + languageRegex);
-				}
-				return emojiOrText;
-			} else {
-				return language;
+			if(languageRegex){
+				if (languageRegex.length >= 1) {
+					languageRegex = languageRegex[0];
+					try {
+						emojiOrText = countryCodeEmoji(languageRegex.split("-")[1]);
+					} catch (err) {
+						emojiOrText = languageRegex;
+						throw new Error("Could not get emoji from language: " + languageRegex);
+					}
+					return emojiOrText;
+				} 
 			}
+			return language;
 		},
 	}
 }
