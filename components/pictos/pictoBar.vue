d815dfd0f0b2d8252f7429245dc94a2000a6c131
UnlikelyBuddy1 <adrianos.sidiras@gmail.com>
2022-03-02 14:00:00 +0100

    avoid pictobar being transparent

--- components/pictos/pictoBar.vue
+++ components/pictos/pictoBar.vue
@@ -1,56 +1,56 @@
 <template>
-	<div class="notification columns is-mobile nopadding" :style="cssVars">
-		<div class="column is-narrow nopadding">
-			<b-button
-				inverted
-				type="is-danger"
-				icon-right="delete"
-				@click="eraseSpeech()"
-			/>
-		</div>
-		<div class="column is-narrow nopadding">
-			<b-button
-				inverted
-				type="is-danger"
-				icon-right="backspace"
-				@click="removeSpeech()"
-			/>
-		</div>
-		<div class="column" style="padding: 0%">
-			<div id="bar" class="scrolling">
-				<miniPicto
-					class
-					v-for="picto in pictos"
-					:key="picto.id + Math.random()"
-					:image="picto.image"
-				/>
-			</div>
-		</div>
-		<div class="column is-narrow nopadding">
-			<b-button
-				v-if="$store.getters.getTemporaryLanguage"
-				type="is-success"
-				icon-right="volume-high"
-				@click="pictalk(pictos)"
-				v-longpress="openTravelerMode"
-				>{{ getEmoji($store.getters.getTemporaryLanguage) }}</b-button
-			>
-			<b-button
-				v-else
-				type="is-success"
-				icon-right="volume-high"
-				@click="pictalk(pictos)"
-				v-longpress="openTravelerMode"
-			></b-button>
-		</div>
-		<div class="column is-narrow nopadding">
-			<b-button
-				type="is-info"
-				icon-right="content-copy"
-				@click="copyPictosToClipboardBase(pictos)"
-			/>
-		</div>
-	</div>
+  <div class="notification columns is-mobile nopadding" :style="cssVars">
+    <div class="column is-narrow nopadding">
+      <b-button
+        inverted
+        type="is-danger"
+        icon-right="delete"
+        @click="eraseSpeech()"
+      />
+    </div>
+    <div class="column is-narrow nopadding">
+      <b-button
+        inverted
+        type="is-danger"
+        icon-right="backspace"
+        @click="removeSpeech()"
+      />
+    </div>
+    <div class="column" style="padding: 0%">
+      <div id="bar" class="scrolling">
+        <miniPicto
+          class
+          v-for="picto in pictos"
+          :key="picto.id + Math.random()"
+          :image="picto.image"
+        />
+      </div>
+    </div>
+    <div class="column is-narrow nopadding">
+      <b-button
+        v-if="$store.getters.getTemporaryLanguage"
+        type="is-success"
+        icon-right="volume-high"
+        @click="pictalk(pictos)"
+        v-longpress="openTravelerMode"
+        >{{ getEmoji($store.getters.getTemporaryLanguage) }}</b-button
+      >
+      <b-button
+        v-else
+        type="is-success"
+        icon-right="volume-high"
+        @click="pictalk(pictos)"
+        v-longpress="openTravelerMode"
+      ></b-button>
+    </div>
+    <div class="column is-narrow nopadding">
+      <b-button
+        type="is-info"
+        icon-right="content-copy"
+        @click="copyPictosToClipboardBase(pictos)"
+      />
+    </div>
+  </div>
 </template>
 <script>
 import axios from "axios";
@@ -63,224 +63,220 @@ import emoji from "@/mixins/emoji";
 import tts from "@/mixins/tts";
 import lang from "@/mixins/lang";
 export default {
-	mixins: [emoji, tts, deviceInfos, lang],
-	methods: {
-		openTravelerMode(e) {
-			if (this.$store.getters.getUser.settings.travelMode) {
-				if (!this.$store.getters.getTemporaryLanguage) {
-					this.$buefy.modal.open({
-						parent: this,
-						component: tradLanguageListVue,
-						hasModalCard: true,
-						customClass: "custom-class custom-class-2",
-						trapFocus: true,
-						canCancel: ["escape", "x"],
-					});
-				} else {
-					this.$store.commit("setTemporaryLanguage", null);
-				}
-			}
-		},
-		getText(pictos) {
-			return pictos.reduce(
-				(acc, curr_val) =>
-					acc + " " + curr_val.speech[this.getUserLang],
-				""
-			);
-		},
-		async copyPictosToClipboardLegacy(pictos) {
-			const message = this.getText(pictos);
-			try {
-				await this.$copyText(message);
-				const notif = this.$buefy.toast.open({
-					duration: 5000,
-					message: this.$t("CopySucces"),
-					type: "is-success",
-					hasIcon: true,
-				});
-			} catch (e) {
-				const notif = this.$buefy.toast.open({
-					duration: 5000,
-					message: this.$t("CopyError"),
-					type: "is-danger",
-					hasIcon: true,
-				});
-			}
-		},
-		async copyPictosToClipboardBase(pictos) {
-			const canWriteToClipboard = await this.askWritePermission();
-			if (canWriteToClipboard) {
-				await this.copyPictosToClipboardV2(pictos);
-			} else {
-				await this.copyPictosToClipboardLegacy(pictos);
-			}
-		},
-		async copyPictosToClipboardV2(pictos) {
-			const paths = pictos.map((picto) => picto.image);
-			const text = this.getText(pictos);
-			const b64 = await mergeImages(paths, {
-				crossOrigin: "Anonymous",
-				text: text,
-				color: "white",
-			});
-			try {
-				const blob = this.b64toBlob(b64);
-				const data = [new ClipboardItem({ [blob.type]: blob })];
-				await navigator.clipboard.write(data);
-				const notif = this.$buefy.toast.open({
-					duration: 5000,
-					message: this.$t("CopySucces"),
-					type: "is-success",
-				});
-			} catch (e) {
-				console.log(e);
-				await this.$copsyText(b64);
-				const notif = this.$buefy.toast.open({
-					duration: 5000,
-					message: this.$t("CopyError"),
-					type: "is-danger",
-				});
-			}
-		},
-		async pictalk(pictos) {
-			this.pronounce(
-				this.getText(pictos),
-				this.getUserLang,
-				this.voiceURI
-			);
-		},
-		removeSpeech() {
-			const pictoSpeech = this.$store.getters.getSpeech;
-			let adminMode = "";
-			if (this.$route.query.isAdmin) {
-				adminMode = "?isAdmin=true";
-			}
-			if (pictoSpeech.length <= 1) {
-				if (this.publicMode) {
-					this.$router.push("/public/");
-				} else {
-					if (this.$store.getters.getRootId) {
-						this.$router.push(
-							"/pictalk/" +
-								this.$store.getters.getRootId +
-								adminMode
-						);
-					} else {
-						this.$router.push("/pictalk" + adminMode);
-					}
-				}
-			} else {
-				if (pictoSpeech[pictoSpeech.length - 2].collection) {
-					this.$router.push(
-						(this.publicMode ? "/public/" : "/pictalk/") +
-							pictoSpeech[pictoSpeech.length - 2].id +
-							adminMode
-					);
-				}
-			}
-			this.$store.commit("removeSpeech");
-		},
-		eraseSpeech() {
-			this.$store.commit("eraseSpeech");
-			const adminMode = this.$route.query.isAdmin ? "?isAdmin=true" : "";
-			if (this.publicMode) {
-				this.$router.push("/public/");
-				return;
-			}
-			if (this.$store.getters.getRootId) {
-				this.$router.push(
-					"/pictalk/" + this.$store.getters.getRootId + adminMode
-				);
-			} else {
-				this.$router.push("/pictalk" + adminMode);
-			}
-		},
-		async askWritePermission() {
-			try {
-				// The clipboard-write permission is granted automatically to pages
-				// when they are the active tab. So it's not required, but it's more safe.
-				const { state } = await navigator.permissions.query({
-					name: "clipboard-write",
-				});
-				return state === "granted";
-			} catch (error) {
-				// Browser compatibility / Security error (ONLY HTTPS) ...
-				return false;
-			}
-		},
-		b64toBlob(dataURI) {
-			const byteString = atob(dataURI.split(",")[1]);
-			const ab = new ArrayBuffer(byteString.length);
-			let ia = new Uint8Array(ab);
+  mixins: [emoji, tts, deviceInfos, lang],
+  methods: {
+    openTravelerMode(e) {
+      if (this.$store.getters.getUser.settings.travelMode) {
+        if (!this.$store.getters.getTemporaryLanguage) {
+          this.$buefy.modal.open({
+            parent: this,
+            component: tradLanguageListVue,
+            hasModalCard: true,
+            customClass: "custom-class custom-class-2",
+            trapFocus: true,
+            canCancel: ["escape", "x"],
+          });
+        } else {
+          this.$store.commit("setTemporaryLanguage", null);
+        }
+      }
+    },
+    getText(pictos) {
+      return pictos.reduce(
+        (acc, curr_val) => acc + " " + curr_val.speech[this.getUserLang],
+        ""
+      );
+    },
+    async copyPictosToClipboardLegacy(pictos) {
+      const message = this.getText(pictos);
+      try {
+        await this.$copyText(message);
+        const notif = this.$buefy.toast.open({
+          duration: 5000,
+          message: this.$t("CopySucces"),
+          type: "is-success",
+          hasIcon: true,
+        });
+      } catch (e) {
+        const notif = this.$buefy.toast.open({
+          duration: 5000,
+          message: this.$t("CopyError"),
+          type: "is-danger",
+          hasIcon: true,
+        });
+      }
+    },
+    async copyPictosToClipboardBase(pictos) {
+      const canWriteToClipboard = await this.askWritePermission();
+      if (canWriteToClipboard) {
+        await this.copyPictosToClipboardV2(pictos);
+      } else {
+        await this.copyPictosToClipboardLegacy(pictos);
+      }
+    },
+    async copyPictosToClipboardV2(pictos) {
+      const paths = pictos.map((picto) => picto.image);
+      const text = this.getText(pictos);
+      const b64 = await mergeImages(paths, {
+        crossOrigin: "Anonymous",
+        text: text,
+        color: "white",
+      });
+      try {
+        const blob = this.b64toBlob(b64);
+        const data = [new ClipboardItem({ [blob.type]: blob })];
+        await navigator.clipboard.write(data);
+        const notif = this.$buefy.toast.open({
+          duration: 5000,
+          message: this.$t("CopySucces"),
+          type: "is-success",
+        });
+      } catch (e) {
+        console.log(e);
+        await this.$copsyText(b64);
+        const notif = this.$buefy.toast.open({
+          duration: 5000,
+          message: this.$t("CopyError"),
+          type: "is-danger",
+        });
+      }
+    },
+    async pictalk(pictos) {
+      this.pronounce(this.getText(pictos), this.getUserLang, this.voiceURI);
+    },
+    removeSpeech() {
+      const pictoSpeech = this.$store.getters.getSpeech;
+      let adminMode = "";
+      if (this.$route.query.isAdmin) {
+        adminMode = "?isAdmin=true";
+      }
+      if (pictoSpeech.length <= 1) {
+        if (this.publicMode) {
+          this.$router.push("/public/");
+        } else {
+          if (this.$store.getters.getRootId) {
+            this.$router.push(
+              "/pictalk/" + this.$store.getters.getRootId + adminMode
+            );
+          } else {
+            this.$router.push("/pictalk" + adminMode);
+          }
+        }
+      } else {
+        if (pictoSpeech[pictoSpeech.length - 2].collection) {
+          this.$router.push(
+            (this.publicMode ? "/public/" : "/pictalk/") +
+              pictoSpeech[pictoSpeech.length - 2].id +
+              adminMode
+          );
+        }
+      }
+      this.$store.commit("removeSpeech");
+    },
+    eraseSpeech() {
+      this.$store.commit("eraseSpeech");
+      const adminMode = this.$route.query.isAdmin ? "?isAdmin=true" : "";
+      if (this.publicMode) {
+        this.$router.push("/public/");
+        return;
+      }
+      if (this.$store.getters.getRootId) {
+        this.$router.push(
+          "/pictalk/" + this.$store.getters.getRootId + adminMode
+        );
+      } else {
+        this.$router.push("/pictalk" + adminMode);
+      }
+    },
+    async askWritePermission() {
+      try {
+        // The clipboard-write permission is granted automatically to pages
+        // when they are the active tab. So it's not required, but it's more safe.
+        const { state } = await navigator.permissions.query({
+          name: "clipboard-write",
+        });
+        return state === "granted";
+      } catch (error) {
+        // Browser compatibility / Security error (ONLY HTTPS) ...
+        return false;
+      }
+    },
+    b64toBlob(dataURI) {
+      const byteString = atob(dataURI.split(",")[1]);
+      const ab = new ArrayBuffer(byteString.length);
+      let ia = new Uint8Array(ab);
 
-			for (var i = 0; i < byteString.length; i++) {
-				ia[i] = byteString.charCodeAt(i);
-			}
-			return new Blob([ab], { type: "image/png" });
-		},
-	},
-	computed: {
-		cssVars() {
-			return {
-				"--bg-color": this.collectionColor,
-			};
-		},
-	},
-	components: {
-		miniPicto: miniPicto,
-	},
-	props: {
-		pictos: {
-			type: Array,
-			required: true,
-		},
-		collectionColor: {
-			type: String,
-			required: true,
-		},
-		publicMode: {
-			type: Boolean,
-			required: false,
-			default: () => false,
-		},
-	},
-	watch: {
-		pictos: function () {
-			setTimeout(() => {
-				let element = document.getElementById("bar");
-				element.scrollLeft = element.scrollWidth;
-			}, 125);
-		},
-	},
-	data() {
-		return {
-			adminMode: false,
-			voices: [],
-			voiceURI: "",
-		};
-	},
+      for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+      return new Blob([ab], { type: "image/png" });
+    },
+  },
+  computed: {
+    cssVars() {
+      return {
+        "--bg-color":
+          this.collectionColor === "#ffffff00"
+            ? "#f5f5f5"
+            : this.collectionColor,
+      };
+    },
+  },
+  components: {
+    miniPicto: miniPicto,
+  },
+  props: {
+    pictos: {
+      type: Array,
+      required: true,
+    },
+    collectionColor: {
+      type: String,
+      required: true,
+    },
+    publicMode: {
+      type: Boolean,
+      required: false,
+      default: () => false,
+    },
+  },
+  watch: {
+    pictos: function () {
+      setTimeout(() => {
+        let element = document.getElementById("bar");
+        element.scrollLeft = element.scrollWidth;
+      }, 125);
+    },
+  },
+  data() {
+    return {
+      adminMode: false,
+      voices: [],
+      voiceURI: "",
+    };
+  },
 };
 </script>
 <style scoped>
 .content {
-	display: flex;
-	align-items: flex-start;
-	justify-content: space-evenly;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-evenly;
 }
 .notification {
-	background-color: var(--bg-color);
-	position: relative;
-	align-items: center;
+  background-color: var(--bg-color);
+  position: relative;
+  align-items: center;
 }
 .nopadding {
-	padding: 0.2rem;
+  padding: 0.2rem;
 }
 .scrolling {
-	display: flex;
-	flex-direction: row;
-	scroll-snap-type: x mandatory;
-	overflow-x: auto;
-	scrollbar-width: thin;
-	scroll-behavior: smooth;
+  display: flex;
+  flex-direction: row;
+  scroll-snap-type: x mandatory;
+  overflow-x: auto;
+  scrollbar-width: thin;
+  scroll-behavior: smooth;
 }
 </style>
