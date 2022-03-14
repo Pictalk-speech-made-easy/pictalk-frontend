1932c10611228f08b3659bf93367ad684ce6aea9
UnlikelyBuddy1 <adrianos.sidiras@gmail.com>
2022-03-14 11:08:16 +0100

    footer problem fixed

--- components/navigation/footer.vue
+++ components/navigation/footer.vue
@@ -1,43 +1,37 @@
 <template>
-	<div>
-		<footer class="footer">
-			<div class="content has-text-centered">
-				<p style="margin-bottom: 0px">
-					<strong>PicTalk</strong> {{ $t("By") }}
-					<a href="https://website.asidiras.dev/"
-						>Alexandros SIDIRAS GALANTE</a
-					>
-					{{ $t("And") }}
-					<a href="https://github.com/UnlikelyBuddy1"
-						>Adrianos SIDIRAS GALANTE</a
-					>. {{ $t("CodeIsLicensed") }}
-					<a href="https://www.gnu.org/licenses/gpl-3.0.en.html"
-						>GNU</a
-					>. {{ $t("TakeALook") }}
-					<nuxt-link to="/legal-infos/terms-of-use">{{
-						$t("TermsOfUse")
-					}}</nuxt-link>
-					{{ $t("And") }}
-					<nuxt-link to="/legal-infos/privacy-policy">{{
-						$t("PrivacyPolicy")
-					}}</nuxt-link
-					>. <b>PicTalk</b> {{ $t("ProjectBackedBy") }}
-				</p>
-				<b-image
-					lazy
-					:srcset="require('@/assets/FondationLogo.png').srcSet"
-					alt="Fondation INSA Lyon logo"
-					class="changeOnViewport"
-				></b-image>
-				{{ $t("WantToBeABacker") }}
-				<a href="mailto:alex@pictalk.org">alex@pictalk.org</a>
-				!
-			</div>
-		</footer>
-	</div>
+  <div class="footers">
+    <div class="content has-text-centered">
+      <p style="margin-bottom: 0px">
+        <strong>PicTalk</strong> {{ $t("By") }}
+        <a href="https://website.asidiras.dev/">Alexandros SIDIRAS GALANTE</a>
+        {{ $t("And") }}
+        <a href="https://github.com/UnlikelyBuddy1">Adrianos SIDIRAS GALANTE</a
+        >. {{ $t("CodeIsLicensed") }}
+        <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU</a>.
+        {{ $t("TakeALook") }}
+        <nuxt-link to="/legal-infos/terms-of-use">{{
+          $t("TermsOfUse")
+        }}</nuxt-link>
+        {{ $t("And") }}
+        <nuxt-link to="/legal-infos/privacy-policy">{{
+          $t("PrivacyPolicy")
+        }}</nuxt-link
+        >. <b>PicTalk</b> {{ $t("ProjectBackedBy") }}
+      </p>
+      <b-image
+        lazy
+        :srcset="require('@/assets/FondationLogo.png').srcSet"
+        alt="Fondation INSA Lyon logo"
+        style="width: 10%"
+      ></b-image>
+      {{ $t("WantToBeABacker") }}
+      <a href="mailto:alex@pictalk.org">alex@pictalk.org</a>
+      !
+    </div>
+  </div>
 </template>
 <style scoped>
-.changeOnViewport {
-	width: 15vmin;
+.footers {
+  padding: 2rem;
 }
 </style>
