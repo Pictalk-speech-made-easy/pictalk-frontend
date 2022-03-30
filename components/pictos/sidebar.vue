f6d7303154c92a42ce7774dc92238003e22dc0bb
UnlikelyBuddy1 <adrianos.sidiras@gmail.com>
2022-03-30 10:36:14 +0200

    sidebar in progress

--- components/pictos/sidebar.vue
+++ components/pictos/sidebar.vue
@@ -1,77 +1,52 @@
 <template>
-	<div
-		class="
-			columns
-			is-multiline is-mobile is-centered
-			notification
-			smallPadding
-		"
-	>
-		<picto
-			class="
-				column
-				is-full-mobile
-				is-half-tablet
-				is-one-quarter-desktop
-				is-one-quarter-widescreen
-				centeredImage
-			"
-			v-for="picto in pictos"
-			:key="picto.id + Math.random()"
-			:picto="picto"
-			:adminMode="adminMode"
-			:publicMode="publicMode"
-			:sidebarMode="true"
-		/>
-		<b-button
-			v-if="adminMode"
-			class="is-success centeredButton"
-			rounded
-			icon-left="image"
-		/>
-		<b-button
-			v-if="adminMode"
-			class="is-danger centeredButton"
-			rounded
-			icon-left="folder-table"
-		/>
-	</div>
+  <div class="columns is-multiline is-mobile notification vertical">
+    <picto
+      class="column is-full-mobile centeredImage smallPadding"
+      v-for="picto in pictos"
+      :key="picto.id + Math.random()"
+      :picto="picto"
+      :adminMode="adminMode"
+      :publicMode="publicMode"
+      :sidebarMode="true"
+    />
+  </div>
 </template>
 <script>
 import picto from "@/components/pictos/picto";
 export default {
-	components: { picto: picto },
-	props: {
-		adminMode: {
-			type: Boolean,
-			required: true,
-		},
-		publicMode: {
-			type: Boolean,
-			required: false,
-			default: () => false,
-		},
-		pictos: {
-			type: Array,
-			required: true,
-		},
-	},
-	data() {
-		return {
-			expanded: false,
-		};
-	},
+  components: { picto: picto },
+  props: {
+    adminMode: {
+      type: Boolean,
+      required: true,
+    },
+    publicMode: {
+      type: Boolean,
+      required: false,
+      default: () => false,
+    },
+    pictos: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      expanded: false,
+    };
+  },
 };
 </script>
 <style scoped>
 .smallPadding {
-	padding: 0px;
+  padding: 5%;
 }
 .centeredImage {
-	align-items: center;
-	justify-content: center;
+  align-items: center;
+  justify-content: center;
 }
-.centeredButton {
-	margin: auto;
+.vertical {
+  flex-direction: row;
+  overflow-y: scroll;
 }
 </style>
--- pages/pictalk/_fatherCollectionId.vue
+++ pages/pictalk/_fatherCollectionId.vue
@@ -1,29 +1,29 @@
 <template>
-	<div>
-		<div class="columns is-mobile">
-			<div class="container is-widescreen column">
-				<pictoList :pictos="loadedPictos" :adminMode="isAdmin" />
-			</div>
-			<sidebar
-				v-if="displaySidebar"
-				v-on:sidebarExpanded="expandOrMinimizeSidebar()"
-				class="sidebar column is-one-quarter smallPadding"
-				:style="expandedOrNot"
-				:adminMode="isAdmin"
-				:publicMode="false"
-				:pictos="loadedSidebarPictos"
-			/>
-		</div>
-		<div class="contenant">
-			<pictoBar
-				v-if="loadSpeech.length != 0"
-				class="pictobar"
-				:pictos="loadSpeech"
-				:collectionColor="collectionColor"
-			/>
-		</div>
-		<!-- <div class="filler"></div> -->
-	</div>
+  <div>
+    <div class="columns is-mobile">
+      <div class="container is-9 column smallPadding noMargins">
+        <pictoList :pictos="loadedPictos" :adminMode="isAdmin" />
+      </div>
+      <sidebar
+        v-if="displaySidebar"
+        v-on:sidebarExpanded="expandOrMinimizeSidebar()"
+        class="sidebar column is-3 smallPadding maxheight"
+        :style="expandedOrNot"
+        :adminMode="isAdmin"
+        :publicMode="false"
+        :pictos="loadedSidebarPictos"
+      />
+    </div>
+    <div class="contenant">
+      <pictoBar
+        v-if="loadSpeech.length != 0"
+        class="pictobar"
+        :pictos="loadSpeech"
+        :collectionColor="collectionColor"
+      />
+    </div>
+    <!-- <div class="filler"></div> -->
+  </div>
 </template>
 <script>
 import axios from "axios";
@@ -31,359 +31,314 @@ import sidebar from "@/components/pictos/sidebar";
 import pictoList from "@/components/pictos/pictoList";
 import pictoBar from "@/components/pictos/pictoBar";
 export default {
-	layout: "pictalk",
-	middleware: ["check-auth", "auth", "axios"],
-	components: {
-		pictoList: pictoList,
-		pictoBar: pictoBar,
-		sidebar: sidebar,
-	},
-	created() {
-		window.addEventListener("online", this.refreshPictos);
-		window.addEventListener("offline", this.lostConnectivityNotification);
-	},
-	destroyed() {
-		window.removeEventListener("online", this.refreshPictos);
-		window.removeEventListener(
-			"offline",
-			this.lostConnectivityNotification
-		);
-	},
-	computed: {
-		displaySidebar() {
-			return (
-				parseInt(this.$route.params.fatherCollectionId, 10) !=
-				this.$store.getters.getSidebarId
-			);
-		},
-		expandedOrNot() {
-			/* return this.sidebarExpanded
+  layout: "pictalk",
+  middleware: ["check-auth", "auth", "axios"],
+  components: {
+    pictoList: pictoList,
+    pictoBar: pictoBar,
+    sidebar: sidebar,
+  },
+  created() {
+    window.addEventListener("online", this.refreshPictos);
+    window.addEventListener("offline", this.lostConnectivityNotification);
+  },
+  destroyed() {
+    window.removeEventListener("online", this.refreshPictos);
+    window.removeEventListener("offline", this.lostConnectivityNotification);
+  },
+  computed: {
+    displaySidebar() {
+      return (
+        parseInt(this.$route.params.fatherCollectionId, 10) !=
+        this.$store.getters.getSidebarId
+      );
+    },
+    expandedOrNot() {
+      /* return this.sidebarExpanded
 				? "margin-right:80%"
 				: "margin-right:25%"; */
-			return "";
-		},
-		loadSpeech() {
-			return this.$store.getters.getSpeech;
-		},
-		isAdmin() {
-			if (this.$route.query.isAdmin) {
-				return true;
-			} else {
-				return false;
-			}
-		},
-		loadedPictos() {
-			return this.loadPictos(
-				parseInt(this.$route.params.fatherCollectionId, 10)
-			);
-		},
-		loadedSidebarPictos() {
-			if (this.$route.query.sidebarPictoId) {
-				return this.loadPictos(
-					parseInt(this.$route.query.sidebarPictoId, 10)
-				);
-			} else {
-				return this.loadPictos(this.$store.getters.getSidebarId);
-			}
-		},
-		collectionColor() {
-			const collection = this.getCollectionFromId(
-				parseInt(this.$route.params.fatherCollectionId, 10)
-			);
-			if (collection) {
-				if (collection.color) {
-					return collection.color;
-				} else {
-					return "#f5f5f5";
-				}
-			} else {
-				return "#f5f5f5";
-			}
-		},
-	},
-	async mounted() {
-		if (!this.$route.params.fatherCollectionId) {
-			if (this.$store.getters.getRootId) {
-				const adminMode = this.$route.query.isAdmin
-					? "?isAdmin=true"
-					: "";
-				this.$router.push(
-					"/pictalk/" + this.$store.getters.getRootId + adminMode
-				);
-				return;
-			} else {
-				var res = await axios.get("/user/root/");
-				this.$store.commit("setRootId", res.data.id);
-				this.$router.push(this.$route.path + "/" + res.data.id);
-			}
-		}
-	},
-	async fetch() {
-		if (!this.$route.params.fatherCollectionId) {
-			return;
-		}
-		const collection = this.getCollectionFromId(
-			parseInt(this.$route.params.fatherCollectionId, 10)
-		);
-		// TODO Traiter differement !collection et !collection.pictos || !collection.collections
-		if (
-			(!collection ||
-				!collection.pictos ||
-				!collection.collections ||
-				collection?.partial) &&
-			navigator.onLine
-		) {
-			try {
-				var res = await axios.get(
-					"/collection/find/" + this.$route.params.fatherCollectionId
-				);
-				if (res.data.image) {
-					res.data.image =
-						this.$config.apiURL +
-						"/image/pictalk/" +
-						res.data.image;
-				}
-				if (res.data.meaning) {
-					res.data.meaning = JSON.parse(res.data.meaning);
-				}
-				if (res.data.speech) {
-					res.data.speech = JSON.parse(res.data.speech);
-				}
-				res.data.collection = true;
-				res.data.partial = false;
+      return "";
+    },
+    loadSpeech() {
+      return this.$store.getters.getSpeech;
+    },
+    isAdmin() {
+      if (this.$route.query.isAdmin) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    loadedPictos() {
+      return this.loadPictos(
+        parseInt(this.$route.params.fatherCollectionId, 10)
+      );
+    },
+    loadedSidebarPictos() {
+      if (this.$route.query.sidebarPictoId) {
+        return this.loadPictos(parseInt(this.$route.query.sidebarPictoId, 10));
+      } else {
+        return this.loadPictos(this.$store.getters.getSidebarId);
+      }
+    },
+    collectionColor() {
+      const collection = this.getCollectionFromId(
+        parseInt(this.$route.params.fatherCollectionId, 10)
+      );
+      if (collection) {
+        if (collection.color) {
+          return collection.color;
+        } else {
+          return "#f5f5f5";
+        }
+      } else {
+        return "#f5f5f5";
+      }
+    },
+  },
+  async mounted() {
+    if (!this.$route.params.fatherCollectionId) {
+      if (this.$store.getters.getRootId) {
+        const adminMode = this.$route.query.isAdmin ? "?isAdmin=true" : "";
+        this.$router.push(
+          "/pictalk/" + this.$store.getters.getRootId + adminMode
+        );
+        return;
+      } else {
+        var res = await axios.get("/user/root/");
+        this.$store.commit("setRootId", res.data.id);
+        this.$router.push(this.$route.path + "/" + res.data.id);
+      }
+    }
+  },
+  async fetch() {
+    if (!this.$route.params.fatherCollectionId) {
+      return;
+    }
+    const collection = this.getCollectionFromId(
+      parseInt(this.$route.params.fatherCollectionId, 10)
+    );
+    // TODO Traiter differement !collection et !collection.pictos || !collection.collections
+    if (
+      (!collection ||
+        !collection.pictos ||
+        !collection.collections ||
+        collection?.partial) &&
+      navigator.onLine
+    ) {
+      try {
+        var res = await axios.get(
+          "/collection/find/" + this.$route.params.fatherCollectionId
+        );
+        if (res.data.image) {
+          res.data.image =
+            this.$config.apiURL + "/image/pictalk/" + res.data.image;
+        }
+        if (res.data.meaning) {
+          res.data.meaning = JSON.parse(res.data.meaning);
+        }
+        if (res.data.speech) {
+          res.data.speech = JSON.parse(res.data.speech);
+        }
+        res.data.collection = true;
+        res.data.partial = false;
 
-				if (res.data.collections && !res.data.collections.length == 0) {
-					res.data.collections.map((collection) => {
-						if (collection.image) {
-							collection.image =
-								this.$config.apiURL +
-								"/image/pictalk/" +
-								collection.image;
-						}
-						if (collection.meaning) {
-							collection.meaning = JSON.parse(collection.meaning);
-						}
-						if (collection.speech) {
-							collection.speech = JSON.parse(collection.speech);
-						}
-						collection.collection = true;
-						collection.fatherCollectionId = res.data.id;
-						if (!collection.pictos) {
-							collection.pictos = [];
-						}
-						if (!collection.collections) {
-							collection.collections = [];
-						}
-						collection.partial = true;
-						// collectionIndex
-						if (!this.getCollectionFromId(collection.id)) {
-							this.$store.commit("addCollection", collection);
-						} else {
-							this.$store.commit("editCollection", collection);
-						}
-					});
-				}
-				if (res.data.pictos && !res.data.pictos.length == 0) {
-					res.data.pictos.map((picto) => {
-						if (picto.image) {
-							picto.image =
-								this.$config.apiURL +
-								"/image/pictalk/" +
-								picto.image;
-						}
-						if (picto.meaning) {
-							picto.meaning = JSON.parse(picto.meaning);
-						}
-						if (picto.speech) {
-							picto.speech = JSON.parse(picto.speech);
-						}
-						picto.fatherCollectionId = res.data.id;
-						if (!this.getPictoFromId(picto.id)) {
-							this.$store.commit("addPicto", picto);
-						} else {
-							this.$store.commit("editPicto", picto);
-						}
-					});
-				}
+        if (res.data.collections && !res.data.collections.length == 0) {
+          res.data.collections.map((collection) => {
+            if (collection.image) {
+              collection.image =
+                this.$config.apiURL + "/image/pictalk/" + collection.image;
+            }
+            if (collection.meaning) {
+              collection.meaning = JSON.parse(collection.meaning);
+            }
+            if (collection.speech) {
+              collection.speech = JSON.parse(collection.speech);
+            }
+            collection.collection = true;
+            collection.fatherCollectionId = res.data.id;
+            if (!collection.pictos) {
+              collection.pictos = [];
+            }
+            if (!collection.collections) {
+              collection.collections = [];
+            }
+            collection.partial = true;
+            // collectionIndex
+            if (!this.getCollectionFromId(collection.id)) {
+              this.$store.commit("addCollection", collection);
+            } else {
+              this.$store.commit("editCollection", collection);
+            }
+          });
+        }
+        if (res.data.pictos && !res.data.pictos.length == 0) {
+          res.data.pictos.map((picto) => {
+            if (picto.image) {
+              picto.image =
+                this.$config.apiURL + "/image/pictalk/" + picto.image;
+            }
+            if (picto.meaning) {
+              picto.meaning = JSON.parse(picto.meaning);
+            }
+            if (picto.speech) {
+              picto.speech = JSON.parse(picto.speech);
+            }
+            picto.fatherCollectionId = res.data.id;
+            if (!this.getPictoFromId(picto.id)) {
+              this.$store.commit("addPicto", picto);
+            } else {
+              this.$store.commit("editPicto", picto);
+            }
+          });
+        }
 
-				if (!this.getCollectionFromId(res.data.id)) {
-					this.$store.commit(
-						"addCollection",
-						JSON.parse(JSON.stringify(res.data))
-					);
-				} else {
-					await this.$store.commit(
-						"editCollection",
-						JSON.parse(JSON.stringify(res.data))
-					);
-				}
-			} catch (error) {
-				console.log("error ", error);
-			}
-		}
-		const user = this.$store.getters.getUser;
-		if (!user.username) {
-			try {
-				await this.$store.dispatch("getUser");
-			} catch (error) {
-				console.log("error ", error);
-			}
-		}
-	},
-	data() {
-		return {
-			isPicto: true,
-			sidebarExpanded: false,
-		};
-	},
-	methods: {
-		loadPictos(fatherCollectionId) {
-			const index = this.$store.getters.getCollections.findIndex(
-				(collection) => collection.id === fatherCollectionId
-			);
-			const collection = this.$store.getters.getCollections[index];
-			if (collection) {
-				const collectionList = collection.collections.map((col) => {
-					return this.getCollectionFromId(col.id);
-				});
-				const pictos = collection.pictos.map((pict) => {
-					return this.getPictoFromId(pict.id);
-				});
-				if (pictos && collectionList) {
-					let sortedItems = [];
-					let starredItems = [];
-					let unstarredItems = [];
-					sortedItems = this.sorting(collectionList, pictos);
-					for (let item of sortedItems) {
-						if (item.starred) {
-							starredItems.push(item);
-						} else {
-							unstarredItems.push(item);
-						}
-					}
-					return starredItems.concat(unstarredItems);
-				} else {
-					console.log("No ranked pictos");
-					return [];
-				}
-			}
-			return [];
-		},
-		expandOrMinimizeSidebar() {
-			this.sidebarExpanded = !this.sidebarExpanded;
-		},
-		sorting(collections, pictos) {
-			let unsortedItems = collections.concat(pictos);
-			let sortedItems = unsortedItems.sort(function (itemA, itemB) {
-				return (
-					new Date(itemB.createdDate) - new Date(itemA.createdDate)
-				);
-			});
-			return sortedItems;
-		},
-		removeSpeech() {
-			this.$store.commit("removeSpeech");
-		},
-		getCollectionFromId(id) {
-			const index = this.$store.getters.getCollections.findIndex(
-				(collection) => collection.id === id
-			);
-			return this.$store.getters.getCollections[index];
-		},
-		getPictoFromId(id) {
-			const index = this.$store.getters.getPictos.findIndex(
-				(picto) => picto.id === id
-			);
-			return this.$store.getters.getPictos[index];
-		},
-		lostConnectivityNotification() {
-			const notif = this.$buefy.notification.open({
-				duration: 5000,
-				message: this.$t("LostConnectivity"),
-				position: "is-top-right",
-				type: "is-danger",
-				hasIcon: true,
-				icon: "airplane",
-			});
-		},
-		async refreshPictos() {
-			try {
-				await this.$store.dispatch("downloadCollections");
-				const notif = this.$buefy.notification.open({
-					duration: 5000,
-					message: this.$t("PictosFetched"),
-					position: "is-top-right",
-					type: "is-success",
-					hasIcon: true,
-					icon: "refresh",
-				});
-			} catch (err) {
-				const notif = this.$buefy.notification.open({
-					duration: 5000,
-					message: this.$t("ServerOffline"),
-					position: "is-top-right",
-					type: "is-danger",
-					hasIcon: true,
-					icon: "danger",
-				});
-			}
-		},
-	},
+        if (!this.getCollectionFromId(res.data.id)) {
+          this.$store.commit(
+            "addCollection",
+            JSON.parse(JSON.stringify(res.data))
+          );
+        } else {
+          await this.$store.commit(
+            "editCollection",
+            JSON.parse(JSON.stringify(res.data))
+          );
+        }
+      } catch (error) {
+        console.log("error ", error);
+      }
+    }
+    const user = this.$store.getters.getUser;
+    if (!user.username) {
+      try {
+        await this.$store.dispatch("getUser");
+      } catch (error) {
+        console.log("error ", error);
+      }
+    }
+  },
+  data() {
+    return {
+      isPicto: true,
+      sidebarExpanded: false,
+    };
+  },
+  methods: {
+    loadPictos(fatherCollectionId) {
+      const index = this.$store.getters.getCollections.findIndex(
+        (collection) => collection.id === fatherCollectionId
+      );
+      const collection = this.$store.getters.getCollections[index];
+      if (collection) {
+        const collectionList = collection.collections.map((col) => {
+          return this.getCollectionFromId(col.id);
+        });
+        const pictos = collection.pictos.map((pict) => {
+          return this.getPictoFromId(pict.id);
+        });
+        if (pictos && collectionList) {
+          let sortedItems = [];
+          let starredItems = [];
+          let unstarredItems = [];
+          sortedItems = this.sorting(collectionList, pictos);
+          for (let item of sortedItems) {
+            if (item.starred) {
+              starredItems.push(item);
+            } else {
+              unstarredItems.push(item);
+            }
+          }
+          return starredItems.concat(unstarredItems);
+        } else {
+          console.log("No ranked pictos");
+          return [];
+        }
+      }
+      return [];
+    },
+    expandOrMinimizeSidebar() {
+      this.sidebarExpanded = !this.sidebarExpanded;
+    },
+    sorting(collections, pictos) {
+      let unsortedItems = collections.concat(pictos);
+      let sortedItems = unsortedItems.sort(function (itemA, itemB) {
+        return new Date(itemB.createdDate) - new Date(itemA.createdDate);
+      });
+      return sortedItems;
+    },
+    removeSpeech() {
+      this.$store.commit("removeSpeech");
+    },
+    getCollectionFromId(id) {
+      const index = this.$store.getters.getCollections.findIndex(
+        (collection) => collection.id === id
+      );
+      return this.$store.getters.getCollections[index];
+    },
+    getPictoFromId(id) {
+      const index = this.$store.getters.getPictos.findIndex(
+        (picto) => picto.id === id
+      );
+      return this.$store.getters.getPictos[index];
+    },
+    lostConnectivityNotification() {
+      const notif = this.$buefy.notification.open({
+        duration: 5000,
+        message: this.$t("LostConnectivity"),
+        position: "is-top-right",
+        type: "is-danger",
+        hasIcon: true,
+        icon: "airplane",
+      });
+    },
+    async refreshPictos() {
+      try {
+        await this.$store.dispatch("downloadCollections");
+        const notif = this.$buefy.notification.open({
+          duration: 5000,
+          message: this.$t("PictosFetched"),
+          position: "is-top-right",
+          type: "is-success",
+          hasIcon: true,
+          icon: "refresh",
+        });
+      } catch (err) {
+        const notif = this.$buefy.notification.open({
+          duration: 5000,
+          message: this.$t("ServerOffline"),
+          position: "is-top-right",
+          type: "is-danger",
+          hasIcon: true,
+          icon: "danger",
+        });
+      }
+    },
+  },
 };
 </script>
 <style scoped>
 .pictobar {
-	bottom: 3%;
-	margin: 0 auto;
-	width: 98%;
-	max-height: 20%;
-	position: fixed;
+  bottom: 3%;
+  margin: 0 auto;
+  width: 98%;
+  max-height: 20%;
+  position: fixed;
 }
 .filler {
-	padding-bottom: 20rem;
+  padding-bottom: 20rem;
 }
 .contenant {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-}
-.fullheight {
-	height: calc(100vh - 64px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .smallPadding {
-	padding: 0px;
+  padding: 0px;
 }
-.sidebar {
-	/* margin-right: 1vw;
-	min-width: 10%;
-	max-width: 10%;
-	min-height: 80vw;
-	margin-top: 5%;
-	float: right; */
-
-	/* position: fixed;
-	top: 5%;
-	right: 1em;
-	min-width: 10vw;
-	max-width: 10vw;
-	min-height: 10vw; */
-	/* position: sticky;
-	display: inline-block;
-	vertical-align: top;
-	height: 80vh;
-	overflow-y: scroll;
-	width: 18vw;
-	max-width: 250px;
-	margin-top: 5%;
-	top: 30px; */
-
-	position: sticky;
-	display: inline-block;
-	vertical-align: top;
-	max-height: 90vh;
-	height: 100%;
-	overflow-y: auto;
-	top: 0;
-	bottom: 0;
+.noMargins {
+  margin: 0%;
+}
+.maxheight {
+  max-height: 525px;
 }
 </style>
