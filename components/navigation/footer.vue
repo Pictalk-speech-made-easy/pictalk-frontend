2bae57c69bbac2cca0cb7ab3ed4ca333abd8aeea
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2022-04-05 13:37:29 +0200

    Footer Names -> Pictalk Speech Made Easy

--- components/navigation/footer.vue
+++ components/navigation/footer.vue
@@ -1,37 +1,38 @@
 <template>
-  <div class="footers">
-    <div class="content has-text-centered">
-      <p style="margin-bottom: 0px">
-        <strong>PicTalk</strong> {{ $t("By") }}
-        <a href="https://website.asidiras.dev/">Alexandros SIDIRAS GALANTE</a>
-        {{ $t("And") }}
-        <a href="https://github.com/UnlikelyBuddy1">Adrianos SIDIRAS GALANTE</a
-        >. {{ $t("CodeIsLicensed") }}
-        <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU</a>.
-        {{ $t("TakeALook") }}
-        <nuxt-link to="/legal-infos/terms-of-use">{{
-          $t("TermsOfUse")
-        }}</nuxt-link>
-        {{ $t("And") }}
-        <nuxt-link to="/legal-infos/privacy-policy">{{
-          $t("PrivacyPolicy")
-        }}</nuxt-link
-        >. <b>PicTalk</b> {{ $t("ProjectBackedBy") }}
-      </p>
-      <b-image
-        lazy
-        :srcset="require('@/assets/FondationLogo.png').srcSet"
-        alt="Fondation INSA Lyon logo"
-        style="width: 10%"
-      ></b-image>
-      {{ $t("WantToBeABacker") }}
-      <a href="mailto:alex@pictalk.org">alex@pictalk.org</a>
-      !
-    </div>
-  </div>
+	<div class="footers">
+		<div class="content has-text-centered">
+			<p style="margin-bottom: 0px">
+				<strong>PicTalk</strong> {{ $t("By") }}
+				<a
+					href="https://www.helloasso.com/associations/pictalk-speech-made-easy"
+					>Pictalk Speech Made Easy</a
+				>
+				. {{ $t("CodeIsLicensed") }}
+				<a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU</a>.
+				{{ $t("TakeALook") }}
+				<nuxt-link to="/legal-infos/terms-of-use">{{
+					$t("TermsOfUse")
+				}}</nuxt-link>
+				{{ $t("And") }}
+				<nuxt-link to="/legal-infos/privacy-policy">{{
+					$t("PrivacyPolicy")
+				}}</nuxt-link
+				>. <b>PicTalk</b> {{ $t("ProjectBackedBy") }}
+			</p>
+			<b-image
+				lazy
+				:srcset="require('@/assets/FondationLogo.png').srcSet"
+				alt="Fondation INSA Lyon logo"
+				style="width: 10%"
+			></b-image>
+			{{ $t("WantToBeABacker") }}
+			<a href="mailto:alex@pictalk.org">alex@pictalk.org</a>
+			!
+		</div>
+	</div>
 </template>
 <style scoped>
 .footers {
-  padding: 2rem;
+	padding: 2rem;
 }
 </style>
