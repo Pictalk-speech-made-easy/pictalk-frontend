6228eeafc7b26679a96103a35b41ef7fffa0b949
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2022-05-02 12:25:47 +0200

    Updated frontpage

--- components/pictos/sidebar.vue
+++ components/pictos/sidebar.vue
@@ -1,135 +1,125 @@
 <template>
-  <div class="vertical smallPadding">
-    <b-button
-      v-if="$route.query.sidebarPictoId"
-      icon-left="arrow-left"
-      expanded
-      class="is-primary"
-      @click="returnWithoutDelete()"
-    />
-    <div class="columns is-multiline is-mobile" style="padding: 5%">
-      <picto
-        class="
-          column
-          is-full-mobile
-          is-full-tablet
-          is-half-desktop
-          is-half-widescreen
-          is-half-fullhd
-          centeredImage
-        "
-        v-for="picto in pictos"
-        :key="picto.id + Math.random()"
-        :picto="picto"
-        :publicMode="publicMode"
-        :sidebarMode="true"
-      ></picto>
-      <div class="column is-full" style="padding: 2rem"></div>
-    </div>
-  </div>
+	<div class="vertical smallPadding">
+		<b-button
+			v-if="$route.query.sidebarPictoId"
+			icon-left="arrow-left"
+			expanded
+			class="is-primary"
+			@click="returnWithoutDelete()"
+		/>
+		<div class="columns is-multiline is-mobile" style="padding: 5%">
+			<picto
+				class="
+					column
+					is-full-mobile
+					is-full-tablet
+					is-half-desktop
+					is-half-widescreen
+					is-half-fullhd
+					centeredImage
+				"
+				v-for="picto in pictos"
+				:key="picto.id + Math.random()"
+				:picto="picto"
+				:publicMode="publicMode"
+				:sidebarMode="true"
+			></picto>
+			<div class="column is-full" style="padding: 2rem"></div>
+		</div>
+	</div>
 </template>
 <script>
 import picto from "@/components/pictos/picto";
 export default {
-  components: { picto: picto },
-  props: {
-    publicMode: {
-      type: Boolean,
-      required: false,
-      default: () => false,
-    },
-    pictos: {
-      type: Array,
-      required: true,
-    },
-  },
-  data() {
-    return {
-      expanded: false,
-    };
-  },
-  methods: {
-    returnWithoutDelete() {
-      const pictoSpeech = this.$store.getters.getSpeech;
-      const sidebarSpeech = this.$store.getters.getSpeech.filter(
-        (picto) => picto.sidebar && picto.collection
-      );
-      if (pictoSpeech.length <= 1) {
-        if (this.publicMode) {
-          this.$router.push("/public/");
-        } else {
-          if (this.$store.getters.getRootId) {
-            this.$router.push({
-              path: "/pictalk/" + this.$store.getters.getRootId,
-              query: { isAdmin: this.$route.query.isAdmin },
-            });
-          } else {
-            this.$router.push({
-              path: "/pictalk",
-              query: { isAdmin: this.$route.query.isAdmin },
-            });
-          }
-        }
-      } else {
-        let prevSidebarPictoIndex = sidebarSpeech.findIndex(
-          (picto) => picto.id == this.$route.query.sidebarPictoId
-        );
-        prevSidebarPictoIndex = sidebarSpeech
-          .slice(0, prevSidebarPictoIndex)
-          .reverse()
-          .findIndex((picto) => picto.collection);
-        console.log(prevSidebarPictoIndex);
-        if (prevSidebarPictoIndex == -1) {
-          if (this.publicMode) {
-            this.$router.push("/public/");
-          } else {
-            if (this.$store.getters.getRootId) {
-              this.$router.push({
-                path: "/pictalk/" + this.$store.getters.getRootId,
-                query: { isAdmin: this.$route.query.isAdmin },
-              });
-            } else {
-              this.$router.push({
-                path: "/pictalk",
-                query: { isAdmin: this.$route.query.isAdmin },
-              });
-            }
-          }
-          return;
-        }
-        if (
-          sidebarSpeech[sidebarSpeech.length - (prevSidebarPictoIndex + 1)]
-            .collection
-        ) {
-          if (sidebarSpeech[sidebarSpeech.length - 2]?.id) {
-            this.$router.push({
-              query: {
-                ...this.$route.query,
-                sidebarPictoId: sidebarSpeech[sidebarSpeech.length - 2].id,
-              },
-            });
-          } else {
-            this.$router.push(this.$route.path);
-          }
-        }
-      }
-    },
-  },
+	components: { picto: picto },
+	props: {
+		publicMode: {
+			type: Boolean,
+			required: false,
+			default: () => false,
+		},
+		pictos: {
+			type: Array,
+			required: true,
+		},
+	},
+	data() {
+		return {
+			expanded: false,
+		};
+	},
+	methods: {
+		returnWithoutDelete() {
+			const pictoSpeech = this.$store.getters.getSpeech;
+			const sidebarSpeech = this.$store.getters.getSpeech.filter(
+				(picto) => picto.sidebar && picto.collection
+			);
+			let prevSidebarPictoIndex = sidebarSpeech.findIndex(
+				(picto) => picto.id == this.$route.query.sidebarPictoId
+			);
+			prevSidebarPictoIndex = sidebarSpeech
+				.slice(0, prevSidebarPictoIndex)
+				.reverse()
+				.findIndex((picto) => picto.collection);
+			console.log(prevSidebarPictoIndex);
+			if (prevSidebarPictoIndex == -1) {
+				if (this.publicMode) {
+					this.$router.push("/public/");
+				} else {
+					if (this.$store.getters.sidebarId) {
+						this.$router.push({
+							path: "/pictalk/" + this.$store.getters.getRootId,
+							query: {
+								isAdmin: this.$route.query.isAdmin,
+								sidebarPictoId: this.$store.getters.sidebarId,
+							},
+						});
+					} else {
+						this.$router.push({
+							path: "/pictalk",
+							query: {
+								isAdmin: this.$route.query.isAdmin,
+								sidebarPictoId: this.$store.getters.sidebarId,
+							},
+						});
+					}
+				}
+				return;
+			}
+			if (
+				sidebarSpeech[
+					sidebarSpeech.length - (prevSidebarPictoIndex + 1)
+				].collection
+			) {
+				if (sidebarSpeech[sidebarSpeech.length - 2]?.id) {
+					this.$router.push({
+						query: {
+							...this.$route.query,
+							sidebarPictoId:
+								sidebarSpeech[sidebarSpeech.length - 2].id,
+						},
+					});
+				} else {
+					this.$router.push(this.$route.path);
+				}
+			}
+		},
+	},
 };
 </script>
 <style scoped>
 .smallPadding {
-  padding: 5%;
+	padding: 5%;
 }
 .centeredImage {
-  align-items: center;
-  justify-content: center;
+	align-items: center;
+	justify-content: center;
 }
 .vertical {
-  overflow-y: scroll;
-  overflow-x: hidden;
-  position: fixed;
-  left: 75%;
-  align-items: center;
+	overflow-y: scroll;
+	overflow-x: hidden;
+	position: fixed;
+	left: 75%;
+	align-items: center;
 }
 </style>
