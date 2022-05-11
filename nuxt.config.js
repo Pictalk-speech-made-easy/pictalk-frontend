9cd1b9c7134e374c2d6e8e79c9424f1a5145ab9e
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2022-05-11 12:26:44 +0200

    Removed vue-virtual-scroller for now but TODO

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,7 +51,7 @@ export default {
   /*
    ** Plugins to load before mounting the App
    */
-  plugins: ["@/plugins/baseURL", "@/plugins/longPress.js", "@/plugins/vueVirtualScroller.js"],
+  plugins: ["@/plugins/baseURL", "@/plugins/longPress.js"],
   /*
    ** Nuxt.js dev-modules
    */
