7bfba3573ea36eef8661b833554ae63513b75aa4
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2022-05-30 18:53:37 +0200

    Choosed user lang is now used before navigator language

--- mixins/lang.js
+++ mixins/lang.js
@@ -1,31 +1,34 @@
 export default {
-	methods: {
-		localeIso() {
-			return this.$i18n.locales.filter(
-				(i) => i.code == this.$i18n.locale
-			)[0].iso;
-		},
-		localeCode() {
-			return this.$i18n.locales.filter(
-				(i) => i.code == this.$i18n.locale
-			)[0].code;
-		},
-	},
-	computed: {
-		getUserLang() {
-			const user = this.$store.getters.getUser;
-			if (user?.language) {
-				return Object.keys(user.language)[0].replace(/[^a-z]/g, "");
-			}
-			if (user?.displayLanguage) {
-				return user.displayLanguage;
-			}
-			return window.navigator.language.replace(/[^a-z]/g, "");
-		},
-		availableLocales() {
-			return this.$i18n.locales.filter(
-				(i) => i.code !== this.$i18n.locale
-			);
-		},
-	}
+  methods: {
+    localeIso() {
+      return this.$i18n.locales.filter(
+        (i) => i.code == this.$i18n.locale
+      )[0].iso;
+    },
+    localeCode() {
+      return this.$i18n.locales.filter(
+        (i) => i.code == this.$i18n.locale
+      )[0].code;
+    },
+  },
+  computed: {
+    getUserLang() {
+      const user = this.$store.getters.getUser;
+      if (user?.language) {
+        return Object.keys(user.language)[0].replace(/[^a-z]/g, "");
+      }
+      if (user?.displayLanguage) {
+        return user.displayLanguage;
+      }
+      if (this.localeCode()) {
+        return this.localeCode();
+      }
+      return window.navigator.language.replace(/[^a-z]/g, "");
+    },
+    availableLocales() {
+      return this.$i18n.locales.filter(
+        (i) => i.code !== this.$i18n.locale
+      );
+    },
+  }
 }
