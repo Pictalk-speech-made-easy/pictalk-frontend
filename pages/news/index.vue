dcb115d72bc889670905ec2fb51dcb5a8da93b84
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2022-10-16 16:15:38 +0200

    More changes

--- pages/news/index.vue
+++ pages/news/index.vue
@@ -12,6 +12,85 @@
         ></iframe>
       </div>
     </section>
+    <section class="section">
+      <div class="container">
+        <h1 class="title">{{ $t("Donations") }}</h1>
+        <h3 class="title is-size-4 is-size-4-mobile">
+          {{ $t("DonationUsage") }}
+        </h3>
+        <div class="columns">
+          <div class="column">
+            <b-image
+              style="aspect-ratio: 16/9; max-width: 15vw"
+              class="img-centered column"
+              lazy
+              src="https://www.aomei.fr/assets/images/misc/pa-server.png"
+              alt="help center people wearing headsets"
+              rounded
+            ></b-image>
+            <h5 class="centered is-size-5 is-size-5-mobile">
+              {{ $t("DonationInformatics") }}: 50€
+            </h5>
+            <div class="centered">{{ $t("DonationInformaticsText") }}</div>
+          </div>
+          <div class="column">
+            <b-image
+              style="aspect-ratio: 16/9; max-width: 15vw"
+              class="img-centered column"
+              lazy
+              src="https://media.istockphoto.com/vectors/bank-building-isolated-on-white-background-vector-illustration-flat-vector-id900791430?k=20&m=900791430&s=612x612&w=0&h=IKTkhxiIwAL6-HMfm9tfW6ti2e4V2roX2IgpUUrbLME="
+              alt="help center people wearing headsets"
+              rounded
+            ></b-image>
+            <h5 class="centered is-size-5 is-size-5-mobile">
+              {{ $t("DonationBank") }}: 15€
+            </h5>
+            <div class="centered">{{ $t("DonationBankText") }}</div>
+          </div>
+          <div class="column">
+            <b-image
+              style="aspect-ratio: 16/9; max-width: 15vw"
+              class="img-centered column"
+              lazy
+              src="https://cdn.iconscout.com/icon/free/png-256/box-with-stuff-2349406-1955397.png"
+              alt="help center people wearing headsets"
+              rounded
+            ></b-image>
+            <h5 class="centered is-size-5 is-size-5-mobile">
+              {{ $t("DonationOther") }}: 8€
+            </h5>
+            <div class="centered">{{ $t("DonationOtherText") }}</div>
+          </div>
+        </div>
+        <h3 class="title is-size-4 is-size-4-mobile">
+          {{ $t("DonationGoals") }}
+        </h3>
+        <div class="columns is-vcentered">
+          <b-image
+            lazy
+            alt="two people lifting the earth"
+            src="https://img.freepik.com/vecteurs-libre/illustration-orthophonie-creative_23-2149237023.jpg?w=2000"
+            style="width: 50%; margin-left: 10%; aspect-ratio: 16/9"
+            class="column"
+          ></b-image>
+          <div class="column">
+            {{ $t("DonationGoalsText1") }}
+          </div>
+        </div>
+        <div class="columns is-vcentered">
+          <b-image
+            lazy
+            alt="two people lifting the earth"
+            src="https://tim-tek.com/wp-content/uploads/2020/08/developer-working-Converti-1170x700.png"
+            style="width: 50%; margin-left: 10%; aspect-ratio: 16/9"
+            class="column"
+          ></b-image>
+          <div class="column">
+            {{ $t("DonationGoalsText2") }}
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 <script >
