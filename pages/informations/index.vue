6068968a33d122f00e9ceeb77cc5b449b2715583
UnlikelyBuddy1 <adrianos.sidiras@gmail.com>
2022-10-20 21:52:49 +0200

    tin j'ai trimé dur

--- pages/informations/index.vue
+++ pages/informations/index.vue
@@ -5,81 +5,86 @@
         {{ $t("Informations") }}
       </h1>
       <h2 class="title is-size-3 is-size-3-mobile">{{ $t("Team") }}</h2>
+ 
+
       <div class="columns is-multiline is-mobile is-centered">
-        <div class="column">
-          <b-image
-            style="aspect-ratio: 16/9; max-width: 15vw"
-            class="img-centered column"
-            lazy
+        <div class="column is-6-mobile is-6-tablette profile">
+          <img
+            class="subcard-img fit-img portrait"
+            loading="lazy"
             src="https://avatars0.githubusercontent.com/u/45072484?s=460&u=319a46de82b5b32ade7afb1807dec076bd6a925f&v=4"
-            alt="help center people wearing headsets"
-            rounded
-          ></b-image>
-          <h5 class="centered is-size-5 is-size-5-mobile">Alex</h5>
+            alt="Photo of Alexandros"
+          ></img>
+          <h5 class="centered is-size-5 is-size-5-mobile"><b>Alex</b></h5>
           <div class="centered">{{ $t("AlexInfos") }}</div>
         </div>
-        <div class="column">
-          <b-image
-            style="aspect-ratio: 16/9; max-width: 15vw"
-            class="img-centered column"
-            lazy
+
+        <div class="column is-6-mobile is-6-tablette profile">
+          <img
+            class="subcard-img fit-img portrait"
+            loading="lazy"
             src="https://avatars.githubusercontent.com/u/52712038?v=4"
-            alt="help center people wearing headsets"
-            rounded
-          ></b-image>
-          <h5 class="centered is-size-5 is-size-5-mobile">Adri</h5>
+            alt="Photo of Adri"
+          ></img>
+          <h5 class="centered is-size-5 is-size-5-mobile"><b>Adri</b></h5>
           <div class="centered">{{ $t("AdriInfos") }}</div>
         </div>
-        <div class="column">
-          <b-image
-            style="aspect-ratio: 16/9; max-width: 15vw"
-            class="img-centered column"
-            lazy
+
+        <div class="column is-6-mobile is-6-tablette profile">
+          <img
+            class="subcard-img fit-img portrait"
+            loading="lazy"
             src="https://avatars.githubusercontent.com/u/91186350?s=400&u=fae6e777f5af5cbfe99c2a73d11ca2fae53e49fb&v=4"
-            alt="help center people wearing headsets"
-            rounded
-          ></b-image>
-          <h5 class="centered is-size-5 is-size-5-mobile">Clement</h5>
+            alt="Photo of Clement"
+          ></img>
+          <h5 class="centered is-size-5 is-size-5-mobile"><b>Clement</b></h5>
           <div class="centered">{{ $t("ClementInfos") }}</div>
         </div>
-        <div class="column">
-          <b-image
-            style="aspect-ratio: 16/9; max-width: 15vw"
-            class="img-centered column"
-            lazy
+
+        <div class="column is-6-mobile is-6-tablette profile">
+          <img
+            class="subcard-img fit-img portrait"
+            loading="lazy"
             src="https://avatars.githubusercontent.com/u/91621093?v=4"
-            alt="help center people wearing headsets"
-            rounded
-          ></b-image>
-          <h5 class="centered is-size-5 is-size-5-mobile">Guillemine</h5>
-          <div class="centered">{{ $t("GuillemineInfos") }}</div>
+            alt="Photo of Guillemine"
+          ></img>
+          <h5 class="centered is-size-5 is-size-5-mobile"><b>Guillemine</b></h5>
+          <div class="centered is-size-5">{{ $t("GuillemineInfos") }}</div>
         </div>
+
       </div>
+
+
     </section>
     <section class="section">
       <h2 class="title is-size-3 is-size-3-mobile">{{ $t("Partners") }}</h2>
       <b-carousel-list
         v-model="test"
         :data="items"
-        :items-to-show="3"
+        :items-to-show="1.5"
+        :breakpoints="breakpoints"
         autoplay
         repeat
+        class="card partners"
       >
         <template #item="list">
           <div class="">
-            <b-image
-              style="aspect-ratio: 16/9; max-width: 20vw"
-              class="center column"
-              lazy
-              :src="list.image"
+            <div class="column subcard">
+              <a :href="list.url">
+              <img
+                class="subcard-img fit-img"
+                loading="lazy"
+                :src="list.image"
               :placeholder="
                 require('@/assets/Contact_compressed.png').placeholder
               "
-              alt="help center people wearing headsets"
-              rounded
-            ></b-image>
-            <p class="title is-4">{{ list.title }}</p>
-            <p class="subtitle is-6">{{ list.description }}</p>
+                alt="logo of our partner"
+              ></img></a>
+              <h5 class="centered is-size-5 is-size-5-mobile">
+                <b>{{ list.title }}</b>
+              </h5>
+              <div class="centered is-size-6 subcard-text">{{ list.description }}</div>
+            </div>
           </div>
         </template>
       </b-carousel-list>
@@ -88,20 +93,19 @@
       <h2 class="title is-size-3 is-size-3-mobile">
         {{ $t("HowContribute") }}
       </h2>
-      <section class="section">
-        <h5 class="title is-size-5 is-size-5-mobile">
-          {{ $t("AreDevelopper") }}
-        </h5>
-        <div class="columns is-vcentered">
-          <b-image
-            lazy
+      <h5 class="title is-size-5 is-size-5-mobile">
+        {{ $t("AreDevelopper") }}
+      </h5>
+      <div class="card">
+        <div class="subcard subcard-row">
+          <img
+            loading="lazy"
             alt="two people lifting the earth"
             :srcset="require('@/assets/webdev.png').srcSet"
             :placeholder="require('@/assets/webdev.png').placeholder"
-            style="width: 50%; margin-left: 10%; aspect-ratio: 16/9"
-            class="column"
-          ></b-image>
-          <div class="column">
+            class="subcard-img bigger"
+          ></img>
+          <div class="subcard-text margin-text is-size-5">
             {{ $t("AreDevelopperText") }} <br />
             <br />Github:
             <a
@@ -111,46 +115,59 @@
             >
           </div>
         </div>
-      </section>
-      <section class="section">
-        <h5 class="title is-size-5 is-size-5-mobile">
-          {{ $t("AreHealthPro") }}
-        </h5>
-        <div class="columns is-vcentered">
-          <b-image
-            lazy
-            class="column"
+      </div>
+      <h5 class="title is-size-5 is-size-5-mobile">
+        {{ $t("AreHealthPro") }}
+      </h5>
+      <div class="card">
+        <div class="subcard subcard-row">
+          <img
+            loading="lazy"
             alt="two people lifting the earth"
             :srcset="require('@/assets/healthpro.png').srcSet"
-            :placeholder="require('@/assets/healthpro.png').placeholder"
-            style="width: 80%; margin-left: 10%; aspect-ratio: 16/9"
-          ></b-image>
-          <div class="column">{{ $t("AreHealthProText") }}</div>
+          :placeholder="require('@/assets/healthpro.png').placeholder"
+            class="subcard-img bigger"
+          ></img>
+          <div class="subcard-text margin-text is-size-5">
+            {{ $t("AreHealthProText") }}
+          </div>
         </div>
-      </section>
-      <section class="section">
-        <h5 class="title is-size-5 is-size-5-mobile">
-          {{ $t("AreParticular") }}
-        </h5>
-        <div class="columns is-vcentered">
-          <b-image
-            lazy
-            class="column"
+      </div>
+      <h5 class="title is-size-5 is-size-5-mobile">
+        {{ $t("AreParticular") }}
+      </h5>
+      <div class="card">
+        <div class="subcard subcard-row">
+          <img
+            loading="lazy"
             alt="two people lifting the earth"
             :srcset="require('@/assets/individual.png').srcSet"
             :placeholder="require('@/assets/individual.png').placeholder"
-            style="width: 80%; margin-left: 10%; aspect-ratio: 16/9"
-          ></b-image>
-          <div class="column">{{ $t("AreParticularText") }}</div>
+            class="subcard-img bigger"
+          ></img>
+          <div class="subcard-text margin-text is-size-5">
+            {{ $t("AreParticularText") }}
+          </div>
         </div>
-      </section>
+      </div>
     </section>
   </div>
 </template>
+
 <script>
 export default {
   data() {
     return {
+      breakpoints: {
+        768: {
+          hasGrayscale: false,
+          itemsToShow: 2.5,
+        },
+        1216: {
+          hasGrayscale: false,
+          itemsToShow: 4,
+        },
+      },
       test: 0,
       items: [
         {
@@ -158,24 +175,28 @@ export default {
           description: this.$t("INSALyonSponsoring"),
           image:
             "https://best.insa-lyon.org/sites/best.insa-lyon.org/files/fondateurs/insa_lyon_foundation.png",
+          url: "https://fondation.insa-lyon.fr/",
         },
         {
           title: "Digital Ocean",
           description: this.$t("DigitalOceanSponsoring"),
           image:
             "https://www.vectorlogo.zone/logos/digitalocean/digitalocean-ar21.png",
+          url: "https://www.digitalocean.com/",
         },
         {
           title: "Adapei 69",
           image:
             "https://espacecolab.adapei69.fr/wp-content/uploads/2020/09/LOGO-ADAPEI_couleur-768x249.jpg",
           description: this.$t("Adapei69Sponsoring"),
+          url: "https://www.adapei69.fr/",
         },
         {
           title: "ARASAAC",
           image:
             "https://aulaabierta.arasaac.org/wp-content/uploads/2020/03/logo_ARASAAC_texto_negro_fondo_transparente.png",
           description: this.$t("ARASAACSponsoring"),
+          url: "https://arasaac.org/",
         },
       ],
     };
@@ -195,5 +216,77 @@ export default {
   display: block;
   margin-left: auto;
   margin-right: auto;
+}
+.section {
+  padding: 6vw;
+  padding-top: 0rem;
+  padding-bottom: 3rem;
+}
+.subcard {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  flex-basis: auto;
+  padding: 1em;
+}
+.profile {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+}
+.subcard-row {
+  flex-direction: row;
+}
+.card {
+  margin: 2vw;
+  border-radius: 12px;
+}
+.subcard-img {
+  border-radius: 12px;
+  aspect-ratio: 1 / 1;
+  max-height: 20vh;
+  object-fit: cover;
+}
+.portrait {
+  border-radius: 100px;
+  margin-bottom: 00.5em;
+}
+.fit-img {
+  object-fit: contain;
+}
+.bigger {
+  max-height: 30vmin;
+  min-height: 200px;
+}
+.centered {
+  text-align: center;
+}
+.subcard-text {
+  margin: 0.4em 0 0em 0;
+}
+.margin-text {
+  margin: 0 1.5vmin 0 3vmin;
+}
+.partners {
+  margin: 0%;
+}
+@media screen and (max-width: 768px) {
+  .subcard-row {
+    flex-direction: column;
+  }
+}
+.portrait:hover {
+  animation: zoom 250ms;
+  animation-fill-mode: forwards;
+}
+@keyframes zoom {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(1.12);
+  }
 }
 </style>
--- pages/news/index.vue
+++ pages/news/index.vue
@@ -1,95 +1,90 @@
 <template>
   <div>
     <section class="section">
-      <div class="container">
+      <div>
         <h1 class="title">{{ $t("News") }}</h1>
         <b-skeleton animated width="80%" :active="!newsCharged"></b-skeleton>
         <b-skeleton animated width="80%" :active="!newsCharged"></b-skeleton>
         <b-skeleton animated width="80%" :active="!newsCharged"></b-skeleton>
-        <iframe
+        <iframe class="card iframe"
           src="https://widget.taggbox.com/110606"
-          style="width: 100%; height: 50vh; border: none"
         ></iframe>
       </div>
     </section>
     <section class="section">
       <div class="container">
         <h1 class="title">{{ $t("Donations") }}</h1>
-        <h3 class="title is-size-4 is-size-4-mobile">
+        <h3 class="title is-size-4 is-size-4-mobile headings">
           {{ $t("DonationUsage") }}
         </h3>
-        <div class="columns">
-          <div class="column">
-            <b-image
-              style="aspect-ratio: 16/9; max-width: 15vw"
-              class="img-centered column"
-              lazy
+        <div class="columns card">
+          <div class="column subcard slot3">
+            <img
+              class="subcard-img"
+              loading="lazy"
               src="https://www.aomei.fr/assets/images/misc/pa-server.png"
-              alt="help center people wearing headsets"
-              rounded
-            ></b-image>
+              alt="drawing of servers to illustrate the costs of running Pictalk application"
+            ></img>
             <h5 class="centered is-size-5 is-size-5-mobile">
-              {{ $t("DonationInformatics") }}: 30€
+              <b>{{ $t("DonationInformatics") }}: 30€</b>
             </h5>
-            <div class="centered">{{ $t("DonationInformaticsText") }}</div>
+            <div class="centered is-size-5 subcard-text">{{ $t("DonationInformaticsText") }}</div>
           </div>
-          <div class="column">
-            <b-image
-              style="aspect-ratio: 16/9; max-width: 15vw"
-              class="img-centered column"
-              lazy
+          <div class="column subcard slot3">
+            <img
+              class="subcard-img"
+              loading="lazy"
               src="https://media.istockphoto.com/vectors/bank-building-isolated-on-white-background-vector-illustration-flat-vector-id900791430?k=20&m=900791430&s=612x612&w=0&h=IKTkhxiIwAL6-HMfm9tfW6ti2e4V2roX2IgpUUrbLME="
-              alt="help center people wearing headsets"
-              rounded
-            ></b-image>
+              alt="drawing of a bank, to illustrate the fees Pictalk pays to have an open bank account and be able to receive donations"
+            ></img>
             <h5 class="centered is-size-5 is-size-5-mobile">
-              {{ $t("DonationBank") }}: 15€
+              <b>{{ $t("DonationBank") }}: 15€</b>
             </h5>
-            <div class="centered">{{ $t("DonationBankText") }}</div>
+            <div class="centered is-size-5 subcard-text">{{ $t("DonationBankText") }}</div>
           </div>
-          <div class="column">
-            <b-image
-              style="aspect-ratio: 16/9; max-width: 15vw"
-              class="img-centered column"
-              lazy
+          <div class="column subcard slot3">
+            <img
+              class="subcard-img"
+              loading="lazy"
               src="https://cdn.iconscout.com/icon/free/png-256/box-with-stuff-2349406-1955397.png"
-              alt="help center people wearing headsets"
-              rounded
-            ></b-image>
+              alt="drawing of a folder with various items, illustrating the fact that we have other fees to pay, such as domain name etc"
+            ></img>
             <h5 class="centered is-size-5 is-size-5-mobile">
-              {{ $t("DonationOther") }}: 8€
+              <b>{{ $t("DonationOther") }}: 8€</b>
             </h5>
-            <div class="centered">{{ $t("DonationOtherText") }}</div>
+            <div class="centered is-size-5 subcard-text">{{ $t("DonationOtherText") }}</div>
           </div>
         </div>
-        <h3 class="title is-size-4 is-size-4-mobile">
+        <h3 class="title is-size-4 is-size-4-mobile headings">
           {{ $t("DonationGoals") }}
         </h3>
-        <div class="columns is-vcentered">
-          <b-image
-            lazy
-            alt="two people lifting the earth"
-            src="https://img.freepik.com/vecteurs-libre/illustration-orthophonie-creative_23-2149237023.jpg?w=2000"
-            style="width: 50%; margin-left: 10%; aspect-ratio: 16/9"
-            class="column"
-          ></b-image>
-          <div class="column">
-            {{ $t("DonationGoalsText1") }}
+        <div class="card">
+          <div class="subcard subcard-row">
+            <img
+              loading="lazy"
+              alt="drawing of woman teaching letters to a child"
+              src="https://img.freepik.com/vecteurs-libre/illustration-orthophonie-creative_23-2149237023.jpg?w=2000"
+              class="subcard-img bigger"
+            ></img>
+            <div class="subcard-text margin-text is-size-5">
+              {{ $t("DonationGoalsText1") }}
+            </div>
           </div>
         </div>
-        <div class="columns is-vcentered">
-          <b-image
-            lazy
-            alt="two people lifting the earth"
-            src="https://tim-tek.com/wp-content/uploads/2020/08/developer-working-Converti-1170x700.png"
-            style="width: 50%; margin-left: 10%; aspect-ratio: 16/9"
-            class="column"
-          ></b-image>
-          <div class="column">
-            {{ $t("DonationGoalsText2") }}
+        <div class="card">
+          <div class="subcard subcard-row">
+            <img
+              loading="lazy"
+              alt="drawing of a person working with a computer"
+              src="https://tim-tek.com/wp-content/uploads/2020/08/developer-working-Converti-1170x700.png"
+              class="subcard-img bigger"
+            ></img>
+            <div class="subcard-text margin-text is-size-5">
+              {{ $t("DonationGoalsText2") }}
+            </div>
           </div>
         </div>
-      </div>
+        </div>
     </section>
   </div>
 </template>
@@ -126,3 +121,66 @@ export default {
   },
 };
 </script>
+<style scoped>
+.section {
+  padding: 6vw;
+  padding-top: 0rem;
+  padding-bottom: 3rem;
+}
+.subcard {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  flex-basis: auto;
+  padding: 1em;
+}
+.subcard-row {
+  flex-direction: row;
+}
+.slot3 {
+  min-width: 33.33%;
+}
+.headings {
+  margin-top: 1em;
+  margin-bottom: 0.5em;
+}
+.card {
+  margin: 2vw;
+  border-radius: 12px;
+}
+.subcard-img {
+  border-radius: 12px;
+  aspect-ratio: 1 / 1;
+  max-height: 20vh;
+  object-fit: cover;
+}
+.bigger {
+  max-height: 30vmin;
+  min-height: 200px;
+}
+.centered {
+  text-align: center;
+}
+.subcard-text {
+  margin: 0.4em 0 0em 0;
+}
+.margin-text {
+  margin: 0 1.5vmin 0 3vmin;
+}
+
+@media screen and (max-width: 768px) {
+  .subcard-row {
+    flex-direction: column;
+  }
+}
+.is-size-5 {
+  padding: 0%;
+}
+.iframe {
+  width: 100%;
+  margin: 0%;
+  height: 50vh;
+  background-color: #f5f5f5;
+}
+</style>
