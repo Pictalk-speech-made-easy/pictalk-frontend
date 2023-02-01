c43ddb64bb5cad617700d9a5f4782c19263628c4
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2023-02-01 19:13:12 +0100

    Addded matomo to Pictalk : without cookies !!!!

--- nuxt.config.js
+++ nuxt.config.js
@@ -173,6 +173,7 @@ export default {
     '@nuxtjs/robots',
     '@nuxtjs/sentry',
     '@nuxtjs/i18n',
+    ['nuxt-matomo', { matomoUrl: '//matomo.home.asidiras.dev/', siteId: 1, cookies: false }]
   ],
   robots: {
     Disallow: [
