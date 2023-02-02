4f5cb1c75b453ec5f66772a93fefbceb136d5be9
UnlikelyBuddy <UnlikelyBuddy@users.noreply.github.com>
2023-02-02 21:09:00 +0100

    mixin and container queries

--- components/pictos/picto.vue
+++ components/pictos/picto.vue
@@ -31,11 +31,11 @@
       <div id="pictogram-image-wrapper" style="width: 100%">
         <div
           v-if="$route.query.isAdmin && !publicMode && !sidebarMode"
-          class="actions"
+          class="actions container"
           @click.self="addToSpeech()"
         >
           <div class="offline subtitle" v-if="!isOnline">
-            {{ $t("LostConnectivity") }}<br />✈️❗
+            <b-icon icon="web-cancel" />
           </div>
           <div
             v-if="isOnline"
@@ -63,7 +63,6 @@
             >
               <b-icon class="large-icon icon" icon="drag"></b-icon>
             </div>
-
             <div v-if="$route.query.isAdmin && !publicMode && !sidebarMode">
               <div v-if="!dragndropId" @click="openActions()">
                 <b-icon class="medium-icon icon" icon="dots-vertical" />
@@ -138,9 +137,7 @@
         />
         <b-skeleton class="skeleton-wrapper" height="100%" :active="skeleton" />
       </div>
-      <div class="meaning">
-        {{ picto.meaning[getUserLang] }}
-      </div>
+      <div class="meaning">{{ picto.meaning[getUserLang] }}</div>
 
       <div
         v-if="
@@ -166,13 +163,12 @@
 import lang from "@/mixins/lang";
 import tts from "@/mixins/tts";
 import deviceInfos from "@/mixins/deviceInfos";
-import deleteItem from "@/components/pictos/deleteItem";
 import PictoSteps from "@/components/pictos/pictoSteps";
-import shareItem from "@/components/pictos/shareItem";
 import pictoActions from "@/components/pictos/pictoActions";
 import LongPress from "vue-directive-long-press";
+import pictogram from "../../mixins/pictogram";
 export default {
-  mixins: [lang, tts, deviceInfos],
+  mixins: [lang, tts, deviceInfos, pictogram],
   name: "picto",
   components: {
     PictoSteps,
@@ -347,189 +343,6 @@ export default {
       ev.target.style.cursor = "grab";
       this.dragImage = ev.target.offsetParent;
     },
-    async setShortcutCollectionIdDirectlyToRoot(collectionId, isPicto) {
-      let collection = JSON.parse(
-        JSON.stringify(this.getCollectionFromId(this.$store.getters.getRootId))
-      );
-      if (isPicto) {
-        collection.pictos.push({ id: collectionId });
-        try {
-          await this.$store.dispatch("editCollection", {
-            id: collection.id,
-            collections: collection.pictos,
-          });
-          const notif = this.$buefy.toast.open({
-            message: this.$t("PublicCopy"),
-            type: "is-success",
-          });
-        } catch (err) {
-          throw new Error(
-            "Could not add shortcut to root collection: " +
-              JSON.stringify(collection.id)
-          );
-        }
-      } else {
-        collection.collections.push({ id: collectionId });
-        try {
-          await this.$store.dispatch("editCollection", {
-            id: collection.id,
-            collections: collection.collections,
-          });
-          const notif = this.$buefy.toast.open({
-            message: this.$t("PublicCopy"),
-            type: "is-success",
-          });
-        } catch (err) {
-          throw new Error(
-            "Could not add shortcut to root collection: " +
-              JSON.stringify(collection.id)
-          );
-        }
-      }
-    },
-    setCopyCollectionId(collectionId, isPicto) {
-      this.$store.commit("setCopyCollectionId", {
-        collectionId: collectionId,
-        isPicto: isPicto,
-      });
-      this.$store.commit("resetShortcutCollectionId");
-    },
-    setShortcutCollectionId(collectionId, isPicto) {
-      this.$store.commit("setShortcutCollectionId", {
-        collectionId: collectionId,
-        isPicto: isPicto,
-      });
-      this.$store.commit("resetCopyCollectionId");
-    },
-    async addToSpeech() {
-      this.$store.commit("addSpeech", {
-        ...this.picto,
-        sidebar: this.sidebarMode,
-      });
-      if (this.picto.collection == true) {
-        if (this.sidebarMode) {
-          this.$router.push({
-            query: {
-              ...this.$route.query,
-              sidebarPictoId: this.picto.id,
-            },
-          });
-        } else {
-          this.$router.push({
-            path: this.pictoLink,
-            query: { ...this.$route.query },
-          });
-        }
-      }
-      if (this.$store.getters.getUser.settings?.pronounceClick) {
-        this.pronounce(
-          this.picto.speech[this.getUserLang],
-          this.getUserLang,
-          this.voiceURI,
-          this.pitch,
-          this.rate
-        );
-      }
-    },
-    deletePicto() {
-      this.$buefy.modal.open({
-        parent: this,
-        props: {
-          object: { ...this.picto },
-        },
-        component: deleteItem,
-        hasModalCard: true,
-        customClass: "custom-class custom-class-2",
-        trapFocus: true,
-        canCancel: ["escape", "x"],
-      });
-    },
-    sharePicto() {
-      this.$buefy.modal.open({
-        parent: this,
-        props: {
-          picto: JSON.parse(JSON.stringify(this.picto)),
-        },
-        component: shareItem,
-        hasModalCard: true,
-        customClass: "custom-class custom-class-2",
-        trapFocus: true,
-        canCancel: ["escape", "x"],
-      });
-    },
-    async publishPicto() {
-      try {
-        this.publishLoad = true;
-        await this.$store.dispatch("publish", this.picto);
-        this.publishLoad = false;
-      } catch (error) {
-        console.log(error);
-        const notif = this.$buefy.notification.open({
-          duration: 4500,
-          message: this.$t("SomeThingBadHappened"),
-          position: "is-top-right",
-          type: "is-danger",
-          hasIcon: true,
-          iconSize: "is-small",
-          icon: "account",
-        });
-      }
-    },
-    editPicto() {
-      this.$buefy.modal.open({
-        parent: this,
-        props: {
-          picto: JSON.parse(JSON.stringify(this.picto)),
-          create: false,
-          isPicto: !this.picto.collection,
-        },
-        component: PictoSteps,
-        hasModalCard: true,
-        customClass: "custom-class custom-class-2",
-        trapFocus: true,
-        canCancel: ["escape", "x"],
-      });
-    },
-    async alternateStar(sign = true, delay = 0) {
-      try {
-        if (this.picto.starred === true) {
-          this.picto.priority = 1;
-        } else if (this.picto.starred === false) {
-          this.picto.priority = 10;
-        }
-        if (sign) {
-          this.picto.priority = this.picto.priority + 1;
-        } else {
-          this.picto.priority = this.picto.priority - 1;
-        }
-        if (this.picto.priority > 10) {
-          this.picto.priority = 1;
-        } else if (this.picto.priority < 1) {
-          this.picto.priority = 10;
-        }
-        await this.$store.dispatch(
-          this.picto.collection
-            ? "alternateCollectionStar"
-            : "alternatePictoStar",
-          {
-            ...this.picto,
-            priority: this.picto.priority,
-          }
-        );
-        $nuxt.$emit("resyncPictoList", delay);
-      } catch (error) {
-        console.log(error);
-        const notif = this.$buefy.notification.open({
-          duration: 4500,
-          message: this.$t("SomeThingBadHappened"),
-          position: "is-top-right",
-          type: "is-danger",
-          hasIcon: true,
-          iconSize: "is-small",
-          icon: "account",
-        });
-      }
-    },
     getCollectionFromId(id) {
       const index = this.$store.getters.getCollections.findIndex(
         (collection) => collection.id === id
@@ -538,18 +351,6 @@ export default {
     },
   },
   computed: {
-    showPriorityOrStarred() {
-      if (this.picto.priority) {
-        return this.picto.priority;
-      } else {
-        return this.picto.starred === true ? 1 : 10;
-      }
-    },
-    colorPriority() {
-      return (
-        "color: hsl(" + (100 - this.showPriorityOrStarred * 10) + ", 100%, 60%)"
-      );
-    },
     isDropZone() {
       return (
         this.dragndropId &&
@@ -569,34 +370,15 @@ export default {
         ? String("/public/" + this.picto.id)
         : String("/pictalk/" + this.picto.id);
     },
-    isToUser() {
-      return this.$store.getters.getUser.id == this.picto.userId;
-    },
-    isEditor() {
-      return (
-        this.picto.editors?.find(
-          (editor) => editor == this.$store.getters.getUser.username
-        ) != undefined
-      );
-    },
-    isViewer() {
-      return (
-        this.picto.viewers?.find(
-          (viewer) => viewer == this.$store.getters.getUser.username
-        ) != undefined
-      );
-    },
-    isOnline() {
-      return window.navigator.onLine;
-    },
   },
 };
 </script>
 <style scoped>
 .offline {
   text-align: center;
-  font-size: 1rem;
+  font-size: 4rem;
   font-weight: 600;
+  color: hsl(0, 100%, 65%);
 }
 .button:focus:not(:active),
 .button.is-focused:not(:active) {
@@ -606,11 +388,12 @@ export default {
   background-color: #ffffff00;
   border: none;
   font-size: 2.5em;
-  text-stroke: 1.5px black;
   text-shadow: 0px 0px 16px #000000d0;
   -webkit-text-stroke: 1.5px black;
   padding: 0;
+  justify-content: end;
   height: 40px;
+  width: 40px;
 }
 
 .priority-button > span > b {
@@ -697,7 +480,51 @@ export default {
 .longpress {
   display: none;
 }
-
+.container {
+  container-type: inline-size;
+}
+@container (max-width: 150px) {
+  .longpress {
+    display: block;
+    position: absolute;
+    bottom: 0px;
+    width: 100%;
+    height: calc(100% - 2rem);
+  }
+  .nopointerevents {
+    pointer-events: none;
+  }
+  .actions > :is(.head-actions) {
+    opacity: 1;
+  }
+  .actions > :is(.main-actions, .foot-actions) {
+    display: none;
+  }
+  .actions:hover {
+    background-color: rgba(0, 0, 0, 0);
+  }
+  .head-actions {
+    padding: 0px;
+    margin-top: -0.1rem;
+  }
+  .priority-button {
+    font-size: 1.75em;
+    height: 32px;
+    width: 32px;
+  }
+  .large-icon {
+    font-size: 2.5rem;
+  }
+  .medium-icon {
+    font-size: 1.75rem;
+  }
+  .icon {
+    text-shadow: 0px 0px 2px #000000d0;
+    -webkit-text-stroke: 1.25px black;
+    transition: 0.075s;
+    cursor: pointer;
+  }
+}
 @media (any-pointer: coarse) {
   .longpress {
     display: block;
@@ -799,6 +626,7 @@ export default {
   padding: 3px;
   position: relative;
   transition: 0.075s ease-in-out;
+  min-width: 180px;
 }
 .pictowrapper:hover {
   z-index: 3;
@@ -845,5 +673,10 @@ export default {
 .skeleton-wrapper {
   width: 100%;
   height: 100%;
+}
+@media screen and (max-width: 1023px) {
+  .pictowrapper {
+    min-width: 105px;
+  }
 }
 </style>
