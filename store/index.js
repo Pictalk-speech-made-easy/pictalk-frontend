427465bd8d294068e3c9f10695430a95c3526456
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2023-03-22 11:50:42 +0100

    Oopsie

--- store/index.js
+++ store/index.js
@@ -465,8 +465,8 @@ export const actions = {
     vuexContext.commit("setToken", res.data.accessToken);
     localStorage.setItem("token", res.data.accessToken);
     localStorage.setItem("tokenExpiration", expDate);
-    Cookie.set("jwt", res.data.accessToken, { sameSite: 'none', secure: true, expirationDate: 604800 });
-    Cookie.set("expirationDate", expDate, { sameSite: 'none', secure: true, expirationDate: 604800 });
+    Cookie.set("jwt", res.data.accessToken, { sameSite: 'none', secure: true, expires: 7 });
+    Cookie.set("expirationDate", expDate, { sameSite: 'none', secure: true, expires: 7 });
     return res;
   },
   initAuth(vuexContext, req) {
@@ -507,10 +507,10 @@ export const actions = {
       vuexContext.commit("setToken", token);
     }
     if (!Cookie.get('jwt') && token) {
-      Cookie.set("jwt", token, { sameSite: 'none', secure: true, expirationDate: 604800 });
+      Cookie.set("jwt", token, { sameSite: 'none', secure: true, expires: 7 });
     }
     if (!Cookie.get('expirationDate') && expirationDate) {
-      Cookie.set("expirationDate", expirationDate, { sameSite: 'none', secure: true, expirationDate: 604800 });
+      Cookie.set("expirationDate", expirationDate, { sameSite: 'none', secure: true, expires: 7 });
     }
   },
   logout(vuexContext) {
