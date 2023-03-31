1fbe48879da4b196f9f102f4312ff17fc5936a65
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2023-03-31 10:24:30 +0200

    Added a video icon to images

--- pages/tutorials/index.vue
+++ pages/tutorials/index.vue
@@ -1,16 +1,17 @@
 <template>
 <div class="container">
   <div class="title">{{$t('Tutorials')}}</div>
-  <div class="columns is-multiline is-vcentered">
+  <div class="columns is-multiline">
   <div class="card column is-one-third">
     <div class="card-image">
       <a href="https://youtu.be/leiYJHOp4QQ">
       <b-image loading="lazy"
-        class="subcard-img bigger"
+        custom-class="image-container subcard-img bigger"
         alt="Presentation image of the youtube video for the basics of pictalk"
         :srcset="require('@/assets/TutorialBasics.png').srcSet"
         :placeholder="require('@/assets/TutorialBasics.png').placeholder"
       />
+      <img class="video-icon" :srcset="require('@/assets/video.png').srcSet">
       </a>
     </div>
     <div class="card-content">
@@ -18,9 +19,11 @@
         <div class="media-left">
           <figure class="image is-48x48">
             <b-image loading="lazy"
+              
               :srcset="require('@/assets/small_logo.png').srcSet"
               :placeholder="require('@/assets/small_logo.png').placeholder"
             />
+            
           </figure>
         </div>
         <div class="media-content">
@@ -36,11 +39,12 @@
     <div class="card-image">
       <a href="https://youtu.be/DO9Dyzjt0Y4">
       <b-image loading="lazy"
-        class="subcard-img bigger"
+        custom-class="subcard-img bigger image-container"
         alt="Presentation image of the youtube video for the basics of pictalk"
         :srcset="require('@/assets/TutorialCreation.png').srcSet"
         :placeholder="require('@/assets/TutorialCreation.png').placeholder"
       />
+      <img class="video-icon" :srcset="require('@/assets/video.png').srcSet">
       </a>
     </div>
     <div class="card-content">
@@ -66,11 +70,12 @@
     <div class="card-image">
       <a href="https://youtu.be/PcB-28gygjY">
       <b-image loading="lazy"
-        class="subcard-img bigger"
+        custom-class="subcard-img bigger image-container"
         alt="Presentation image of the youtube video for the basics of pictalk"
         :srcset="require('@/assets/TutorialOrganizing.png').srcSet"
         :placeholder="require('@/assets/TutorialOrganizing.png').placeholder"
       />
+      <img class="video-icon" :srcset="require('@/assets/video.png').srcSet">
       </a>
     </div>
     <div class="card-content">
@@ -96,11 +101,12 @@
     <div class="card-image">
       <a href="https://youtu.be/iDvX3BWYadc">
       <b-image loading="lazy"
-        class="subcard-img bigger"
+        custom-class="subcard-img bigger image-container"
         alt="Presentation image of the youtube video for the basics of pictalk"
         :srcset="require('@/assets/TutorialSharing.png').srcSet"
         :placeholder="require('@/assets/TutorialSharing.png').placeholder"
       />
+      <img class="video-icon" :srcset="require('@/assets/video.png').srcSet">
       </a>
     </div>
     <div class="card-content">
@@ -132,5 +138,25 @@
 
 .bigger {
   min-height: 30vmin;
+}
+.image-container {
+  position: relative;
+  display: inline-block;
+}
+
+.image-container img {
+  display: block;
+}
+.video-icon {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1;
+  width: 50px;
+  opacity: 0.8;
+}
+.video-icon:hover {
+  opacity: 1;
 }
 </style>
