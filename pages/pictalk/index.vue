ae0f6bce076dba95a827aa9323a1745ab622e92c
UnlikelyBuddy1 <adrianos.sidiras@gmail.com>
2026-02-18 16:40:45 +0100

    removed unused

--- pages/pictalk/index.vue
+++ pages/pictalk/index.vue
@@ -224,51 +224,19 @@ export default {
     },
   },
   async mounted() {
-    this.$buefy.modal.open({
-      parent: this,
-      props: {
-        campaign: this.$store.getters.getCampaign,
+    // this.$buefy.modal.open({
+    //   parent: this,
+    //   props: {
+    //     campaign: this.$store.getters.getCampaign,
 
-      },
-      component: MembershipModal,
-      hasModalCard: true,
-      customClass: "custom-class custom-class-2",
-      trapFocus: true,
-      fullScreen: true,
-      canCancel: []
-    });
-    /* setTimeout(() => {
-      this.$buefy.modal.open({
-        parent: this,
-        props: {
-          campaign: this.$store.getters.getCampaign,
-
-        },
-        component: MembershipFollowupModal,
-        hasModalCard: true,
-        customClass: "custom-class custom-class-2",
-        trapFocus: true,
-        fullScreen: true,
-        canCancel: []
-      });
-    }, 500); */
-
-    /* setTimeout(() => {
-      this.$buefy.modal.open({
-        parent: this,
-        props: {
-          campaign: this.$store.getters.getCampaign,
-          donationArray: this.$store.getters.getDonationPanel,
-          suggestedPrompts: this.$store.getters.getSuggestedPrompts
-        },
-        component: DonationModal,
-        hasModalCard: true,
-        customClass: "custom-class custom-class-2",
-        trapFocus: true,
-        fullScreen: true,
-        canCancel: []
-      });
-    }, 500); */
+    //   },
+    //   component: MembershipModal,
+    //   hasModalCard: true,
+    //   customClass: "custom-class custom-class-2",
+    //   trapFocus: true,
+    //   fullScreen: true,
+    //   canCancel: []
+    // });
     let query = { ...this.$route.query };
     if (
       !this.$route.query.fatherCollectionId
