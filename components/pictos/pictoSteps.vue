2730b898f7623a0688ec14c858363a3bb6a3dd44
Alexandros SIDIRAS <alexandros.sidiras@laposte.net>
2026-06-01 16:47:49 +0200

    prevent from showing the modal too frequently on device

--- components/pictos/pictoSteps.vue
+++ components/pictos/pictoSteps.vue
@@ -135,13 +135,13 @@
             <img v-if="!rendered" :src="dynamicSrc" class="optionImage" />
             <b-field style="padding-top: 0.8rem">
               <b-switch v-model="options.cross.enabled" type="is-success" class="optionSwitch">{{ $t("Negation")
-              }}</b-switch>
+                }}</b-switch>
               <b-switch v-model="options.arrow.enabled" type="is-success" class="optionSwitch">{{ $t("Time")
-              }}</b-switch>
+                }}</b-switch>
               <b-switch v-model="options.plus.enabled" type="is-success" class="optionSwitch">{{ $t("Plural")
-              }}</b-switch>
+                }}</b-switch>
               <b-switch v-model="options.question.enabled" type="is-success" class="optionSwitch">{{ $t("Question")
-              }}</b-switch>
+                }}</b-switch>
             </b-field>
             <b-field>
               <b-button icon-left="refresh" :label="$t('Rotation')" @click="rotateImg()"></b-button>
@@ -651,53 +651,61 @@ export default {
       }
       this.creationLoading = false;
       this.$emit("close");
-      if (this.$store.getters.getSuggestedPrompts.prompts.donation && user_type_modal.seen === false) {
-        setTimeout(() => {
-          this.$buefy.modal.open({
-            parent: this,
-            props: {
-              campaign: this.$store.getters.getCampaign,
-              donationArray: this.$store.getters.getDonationPanel,
-              suggestedPrompts: this.$store.getters.getSuggestedPrompts
-            },
-            component: DonationModal,
-            hasModalCard: true,
-            customClass: "custom-class custom-class-2",
-            trapFocus: true,
-            fullScreen: true,
-            canCancel: []
-          });
-        }, 1500);
-      } else if (this.$store.getters.getSuggestedPrompts.prompts.membership) {
-        setTimeout(() => {
-          this.$buefy.modal.open({
-            parent: this,
-            props: {
-              campaign: this.$store.getters.getCampaign,
-            },
-            component: MembershipModal,
-            hasModalCard: true,
-            customClass: "custom-class custom-class-2",
-            trapFocus: true,
-            fullScreen: true,
-            canCancel: []
-          });
-        }, 1500);
-      } else if (this.$store.getters.getSuggestedPrompts.prompts.followupMembership) {
-        setTimeout(() => {
-          this.$buefy.modal.open({
-            parent: this,
-            props: {
-              campaign: this.$store.getters.getCampaign,
-            },
-            component: MembershipFollowupModal,
-            hasModalCard: true,
-            customClass: "custom-class custom-class-2",
-            trapFocus: true,
-            fullScreen: true,
-            canCancel: []
-          });
-        }, 1500);
+      const lastShown = this.$store.getters.getModalLastShownAt;
+      const oneMonth = 30 * 24 * 60 * 60 * 1000;
+      const canShowModal = !lastShown || (Date.now() - lastShown) > oneMonth;
+      if (canShowModal) {
+        if (this.$store.getters.getSuggestedPrompts.prompts.donation && user_type_modal.seen === false) {
+          this.$store.commit("setModalLastShownAt", Date.now());
+          setTimeout(() => {
+            this.$buefy.modal.open({
+              parent: this,
+              props: {
+                campaign: this.$store.getters.getCampaign,
+                donationArray: this.$store.getters.getDonationPanel,
+                suggestedPrompts: this.$store.getters.getSuggestedPrompts
+              },
+              component: DonationModal,
+              hasModalCard: true,
+              customClass: "custom-class custom-class-2",
+              trapFocus: true,
+              fullScreen: true,
+              canCancel: []
+            });
+          }, 1500);
+        } else if (this.$store.getters.getSuggestedPrompts.prompts.membership) {
+          this.$store.commit("setModalLastShownAt", Date.now());
+          setTimeout(() => {
+            this.$buefy.modal.open({
+              parent: this,
+              props: {
+                campaign: this.$store.getters.getCampaign,
+              },
+              component: MembershipModal,
+              hasModalCard: true,
+              customClass: "custom-class custom-class-2",
+              trapFocus: true,
+              fullScreen: true,
+              canCancel: []
+            });
+          }, 1500);
+        } else if (this.$store.getters.getSuggestedPrompts.prompts.followupMembership) {
+          this.$store.commit("setModalLastShownAt", Date.now());
+          setTimeout(() => {
+            this.$buefy.modal.open({
+              parent: this,
+              props: {
+                campaign: this.$store.getters.getCampaign,
+              },
+              component: MembershipFollowupModal,
+              hasModalCard: true,
+              customClass: "custom-class custom-class-2",
+              trapFocus: true,
+              fullScreen: true,
+              canCancel: []
+            });
+          }, 1500);
+        }
       }
     },
     async traductionNeeded() {
